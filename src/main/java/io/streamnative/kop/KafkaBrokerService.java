16052ccb328e52840367904de510ed76dd763704
Sijie Guo <sijie@apache.org>
2019-07-12 21:45:51 -0700

    Use reflection to access private fields and methods of BrokerService (#5)
    *Motivation*
    
    Use a customized version slows down the development.
    
    *Modifications*
    
    - Change to use reflection to access private fields and methods
    - Add license headers
    - Delete unneeded filees
    
    *Verify this change*
    
    - Enable CI

--- src/main/java/io/streamnative/kop/KafkaBrokerService.java
+++ src/main/java/io/streamnative/kop/KafkaBrokerService.java
@@ -1,32 +1,28 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.streamnative.kop;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelOption;
+import io.streamnative.kop.utils.ReflectionUtils;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.broker.service.BrokerService;
+import org.apache.pulsar.broker.service.BrokerServiceUtil;
 import org.apache.pulsar.broker.service.DistributedIdGenerator;
-import org.apache.pulsar.broker.zookeeper.aspectj.ClientCnxnAspect;
 import org.apache.pulsar.common.allocator.PulsarByteBufAllocator;
 import org.apache.pulsar.common.util.netty.EventLoopUtil;
 
@@ -48,15 +44,23 @@ public class KafkaBrokerService extends BrokerService {
     public void start() throws Exception {
         KafkaServiceConfiguration serviceConfig = kafkaService.getKafkaConfig();
 
-        setProducerNameGenerator(new DistributedIdGenerator(kafkaService.getZkClient(), producerNameGeneratorPath,
-            serviceConfig.getClusterName()));
+        DistributedIdGenerator producerNameGenerator =
+            new DistributedIdGenerator(
+                kafkaService.getZkClient(),
+                "/counters/producer-name",
+                kafkaService.getConfiguration().getClusterName());
+        ReflectionUtils.setField(this, "producerNameGenerator", producerNameGenerator);
 
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.childOption(ChannelOption.ALLOCATOR, PulsarByteBufAllocator.DEFAULT);
-        bootstrap.group(getAcceptorGroup(), getWorkerGroup());
+        bootstrap.group(
+            ReflectionUtils.getField(this, "acceptorGroup"),
+            ReflectionUtils.getField(this, "workerGroup"));
         bootstrap.childOption(ChannelOption.TCP_NODELAY, true);
 
-        bootstrap.channel(EventLoopUtil.getServerSocketChannelClass(getWorkerGroup()));
+        bootstrap.channel(EventLoopUtil.getServerSocketChannelClass(
+            ReflectionUtils.getField(this, "workerGroup")
+        ));
         EventLoopUtil.enableTriggeredMode(bootstrap);
 
         bootstrap.childHandler(new KafkaChannelInitializer(kafkaService, false));
@@ -74,12 +78,21 @@ public class KafkaBrokerService extends BrokerService {
         }
 
         // start other housekeeping functions
-        this.startStatsUpdater(
+        BrokerServiceUtil.startStatsUpdater(
+            this,
             serviceConfig.getStatsUpdateInitialDelayInSecs(),
             serviceConfig.getStatsUpdateFrequencyInSecs());
-        this.startInactivityMonitor();
-        this.startMessageExpiryMonitor();
-        this.startCompactionMonitor();
-        this.startBacklogQuotaChecker();
+        ReflectionUtils.callNoArgVoidMethod(
+            this, "startInactivityMonitor"
+        );
+        ReflectionUtils.callNoArgVoidMethod(
+            this, "startMessageExpiryMonitor"
+        );
+        ReflectionUtils.callNoArgVoidMethod(
+            this, "startCompactionMonitor"
+        );
+        ReflectionUtils.callNoArgVoidMethod(
+            this, "startBacklogQuotaChecker"
+        );
     }
 }
