7d6928c50e18a5cf1ace55f28208a420408e4245
Yunze Xu <xyzinfernity@163.com>
2020-10-18 13:28:19 +0800

    Add kafka-client docker images

--- integrations/build.sh
+++ integrations/build.sh
@@ -20,11 +20,24 @@ VERSION=$(${INTR_HOME}/scripts/dev/get-project-version.py)
 TAG=${VERSION%"-SNAPSHOT"}
 IMAGE_NAME_PREFIX="kop-test-"
 
-for img_dir in `ls -d ${INTR_HOME}/integrations/*/ | grep -v dev`; do
-    BASE_NAME=$(basename ${img_dir})
-    cd ${img_dir}
-    IMAGE="streamnative/${IMAGE_NAME_PREFIX}${BASE_NAME}:${TAG}"
+build_image() {
+    IMAGE=$1
     echo "Building test image : ${IMAGE}"
     docker build . -t ${IMAGE}
     echo "Successfully built test image : ${IMAGE}"
+}
+
+for img_dir in `ls -d ${INTR_HOME}/integrations/*/ | grep -v dev`; do
+    BASE_NAME=$(basename ${img_dir})
+    cd ${img_dir}
+    if [[ $BASE_NAME == "kafka-client" ]]; then
+        VERSIONS=(1.0.0 1.1.0 2.0.0 2.1.0 2.2.0 2.3.0 2.4.0 2.5.0 2.6.0)
+        for VERSION in ${VERSIONS[@]}; do
+            sed -i '' "s/<kafka\.version>.*<\/kafka\.version>/<kafka.version>$VERSION<\/kafka.version>/" pom.xml
+            build_image "streamnative/${IMAGE_NAME_PREFIX}${BASE_NAME}-${VERSION}:${TAG}"
+            sed -i '' "s/<kafka\.version>.*<\/kafka\.version>/<kafka.version>2.6.0<\/kafka.version>/" pom.xml
+        done
+    else
+        build_image "streamnative/${IMAGE_NAME_PREFIX}${BASE_NAME}:${TAG}"
+    fi
 done
