6a76be0eeeb47ab50a8f4081a5a36ca342404566
Yunze Xu <xyzinfernity@163.com>
2020-10-23 18:57:21 +0800

    Add KopTopic

---
+++ kafka-impl/src/test/java/io/streamnative/pulsar/handlers/kop/utils/KopTopicTest.java
@@ -0,0 +1,77 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop.utils;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import org.testng.annotations.Test;
+
+/**
+ * Test for KopTopic.
+ */
+public class KopTopicTest {
+
+    @Test
+    public void testConstructor() {
+        KopTopic topic;
+        try {
+            topic = new KopTopic("my-topic");
+            fail();
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "KopTopic is not initialized");
+        }
+
+        KopTopic.initialize("my-tenant/my-ns");
+
+        topic = new KopTopic("my-topic");
+        assertEquals(topic.getOriginalName(), "my-topic");
+        assertEquals(topic.getFullName(), "persistent://my-tenant/my-ns/my-topic");
+
+        topic = new KopTopic("my-tenant-2/my-ns-2/my-topic");
+        assertEquals(topic.getOriginalName(), "my-tenant-2/my-ns-2/my-topic");
+        assertEquals(topic.getFullName(), "persistent://my-tenant-2/my-ns-2/my-topic");
+
+        topic = new KopTopic("persistent://my-tenant-3/my-ns-3/my-topic");
+        assertEquals(topic.getOriginalName(), "persistent://my-tenant-3/my-ns-3/my-topic");
+        assertEquals(topic.getFullName(), topic.getOriginalName());
+
+        try {
+            topic = new KopTopic("my-ns/my-topic");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().contains("Invalid short topic name"));
+        }
+        try {
+            topic = new KopTopic("persistent://my-topic");
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().contains("Invalid topic name"));
+        }
+    }
+
+    @Test
+    public void testGetPartitionName() {
+        KopTopic.initialize("my-tenant/my-ns");
+        KopTopic topic = new KopTopic("my-topic");
+        assertEquals(topic.getPartitionName(0), "persistent://my-tenant/my-ns/my-topic-partition-0");
+        assertEquals(topic.getPartitionName(12), "persistent://my-tenant/my-ns/my-topic-partition-12");
+        try {
+            topic.getPartitionName(-1);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().contains("Invalid partition"));
+        }
+    }
+}
