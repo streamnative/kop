a1c8862ef13e8171c30ff0765a6be7b5ac553ac2
dockerzhang <dockerzhang@apache.org>
2020-11-02 11:31:20 +0800

    fix fetch offset when topic is null (#203)
    bug fix for #202

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -740,7 +740,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         KeyValue<Errors, Map<TopicPartition, OffsetFetchResponse.PartitionData>> keyValue =
             groupCoordinator.handleFetchOffsets(
                 request.groupId(),
-                Optional.of(request.partitions())
+                Optional.ofNullable(request.partitions())
             );
 
         resultFuture.complete(new OffsetFetchResponse(keyValue.getKey(), keyValue.getValue()));
