19e0e0e04659c9ac4671ff3ac5ff9e33152be169
dockerzhang <dockerzhang@apache.org>
2020-11-03 17:16:59 +0800

    fix ut failed

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaApisTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaApisTest.java
@@ -498,7 +498,7 @@ public class KafkaApisTest extends KopProtocolHandlerTestBase {
 
         int messagesPerPartition = 9;
         int maxResponseBytes = 800;
-        int maxPartitionBytes = 190;
+        int maxPartitionBytes = 900;
 
         List<TopicPartition> topicPartitions = createTopics(topicName, numberTopics, numberPartitions);
 
