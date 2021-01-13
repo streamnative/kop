7d103e8e63118c764bcee9917a7cb1d765a10e50
Yunze Xu <xyzinfernity@163.com>
2021-01-13 21:08:27 +0800

    Add EndPoint implementation

---
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/EndPoint.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lombok.Getter;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
+
+/**
+ * The endpoint that KoP binds.
+ */
+public class EndPoint {
+
+    private static final String REGEX = "^(.*)://\\[?([0-9a-zA-Z\\-%._:]*)\\]?:(-?[0-9]+)";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+
+    @Getter
+    private final SecurityProtocol securityProtocol;
+    @Getter
+    private final String hostname;
+    @Getter
+    private final int port;
+
+    public EndPoint(final String listener) {
+        final String errorMessage = "listener '" + listener + "' is invalid";
+        final Matcher matcher = PATTERN.matcher(listener);
+        checkState(matcher.find(), errorMessage);
+        checkState(matcher.groupCount() == 3, errorMessage);
+
+        this.securityProtocol = SecurityProtocol.forName(matcher.group(1));
+        final String originalHostname = matcher.group(2);
+        if (originalHostname.isEmpty()) {
+            try {
+                this.hostname = InetAddress.getLocalHost().getCanonicalHostName();
+            } catch (UnknownHostException e) {
+                throw new IllegalStateException("hostname is empty and localhost is unknown: " + e.getMessage());
+            }
+        } else {
+            this.hostname = originalHostname;
+        }
+        this.port = Integer.parseInt(matcher.group(3));
+        checkState(port >= 0 && port < 65535, errorMessage + ": port " + port + " is invalid");
+    }
+
+    public String getConnectionString() {
+        return hostname + ":" + port;
+    }
+}
---
+++ kafka-impl/src/test/java/io/streamnative/pulsar/handlers/kop/EndPointTest.java
@@ -0,0 +1,67 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.net.InetAddress;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for EndPoint.
+ */
+public class EndPointTest {
+
+    @Test
+    public void testValidListener() throws Exception {
+        EndPoint endPoint = new EndPoint("PLAINTEXT://192.168.0.1:9092");
+        assertEquals(endPoint.getSecurityProtocol(), SecurityProtocol.PLAINTEXT);
+        assertEquals(endPoint.getHostname(), "192.168.0.1");
+        assertEquals(endPoint.getPort(), 9092);
+
+        final String localhost = InetAddress.getLocalHost().getCanonicalHostName();
+        endPoint = new EndPoint("SSL://:9094");
+        assertEquals(endPoint.getSecurityProtocol(), SecurityProtocol.SSL);
+        assertEquals(endPoint.getHostname(), localhost);
+        assertEquals(endPoint.getPort(), 9094);
+    }
+
+    @Test
+    public void testInvalidListener() throws Exception {
+        try {
+            new EndPoint("hello world");
+        } catch (IllegalStateException e) {
+            assertTrue(e.getMessage().contains("listener 'hello world' is invalid"));
+            e.getMessage();
+        }
+        try {
+            new EndPoint("pulsar://localhost:6650");
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().contains("No enum constant"));
+        }
+        try {
+            new EndPoint("PLAINTEXT://localhost:123456");
+        } catch (IllegalStateException e) {
+            assertTrue(e.getMessage().contains("port 123456 is invalid"));
+        }
+        try {
+            new EndPoint("PLAINTEXT://localhost:-1");
+        } catch (IllegalStateException e) {
+            assertTrue(e.getMessage().contains("port -1 is invalid"));
+        }
+    }
+
+}
