6673181334d735cc97eed45a94a3a68c78e994fc
hangc0276 <hangc0276@163.com>
2021-01-26 19:36:16 +0800

    fix tests run failed

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/coordinator/group/GroupMetadataManagerTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/coordinator/group/GroupMetadataManagerTest.java
@@ -58,6 +58,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.common.util.MathUtils;
 import org.apache.bookkeeper.common.util.OrderedScheduler;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.protocol.Errors;
@@ -847,7 +848,8 @@ public class GroupMetadataManagerTest extends KopProtocolHandlerTestBase {
         // group is not owned
         assertFalse(groupMetadataManager.groupNotExists(groupId));
 
-        groupMetadataManager.addPartitionOwnership(groupPartitionId);
+        int tmpGroupPartitionId = MathUtils.signSafeMod(groupId.hashCode(), offsetConfig.offsetsTopicNumPartitions());
+        groupMetadataManager.addPartitionOwnership(tmpGroupPartitionId);
         // group is owned but does not exist yet
         assertTrue(groupMetadataManager.groupNotExists(groupId));
 
