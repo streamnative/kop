a6d99cc6e8b2aa13da4691757e2a218dc85fe468
Yunze Xu <xyzinfernity@163.com>
2021-01-27 09:42:51 +0800

    Update kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/MessageFetchContext.java
    Remove unused import

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/MessageFetchContext.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/MessageFetchContext.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
