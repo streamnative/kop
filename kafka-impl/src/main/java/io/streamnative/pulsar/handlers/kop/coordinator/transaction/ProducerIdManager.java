49dde4c77ff2259eea873b3762a7ad10a4c029c4
gaoran10 <gaoran_10@126.com>
2021-02-03 18:26:32 +0800

    fix check style

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/ProducerIdManager.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/ProducerIdManager.java
@@ -13,13 +13,8 @@
  */
 package io.streamnative.pulsar.handlers.kop.coordinator.transaction;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicLong;
-import lombok.AllArgsConstructor;
-import org.apache.zookeeper.AsyncCallback;
-import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
 
 /**
  * This class used to manage producer id.
@@ -27,37 +22,13 @@ import org.apache.zookeeper.data.Stat;
 public class ProducerIdManager {
 
     private final AtomicLong producerId = new AtomicLong(0);
-    private final String pidBlockZNode = "/kop_latest_producer_id_block";
-    private final ZooKeeper zkClient;
 
     public ProducerIdManager(ZooKeeper zkClient) {
-        this.zkClient = zkClient;
     }
 
     public long generateProducerId() {
         // TODO generate unique producer id
         return producerId.incrementAndGet();
-    }
-
-    public CompletableFuture<DataAndVersion> getPidDataAndVersion() {
-        CompletableFuture<DataAndVersion> future = new CompletableFuture<>();
-        zkClient.getData(pidBlockZNode, null, new AsyncCallback.DataCallback() {
-            @Override
-            public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
-                if (rc != KeeperException.Code.OK.intValue()) {
-                    future.complete(new DataAndVersion(data, stat.getVersion()));
-                } else {
-                    future.complete(null);
-                }
-            }
-        }, null);
-        return future;
-    }
-
-    @AllArgsConstructor
-    private static class DataAndVersion {
-        private byte[] data;
-        private int zkVersion;
     }
 
 }
