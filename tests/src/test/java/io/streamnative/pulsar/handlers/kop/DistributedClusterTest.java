44376230548b2242f8d513ddcedbd468651504d1
hangc0276 <hangc0276@163.com>
2021-03-26 21:24:17 +0800

    disable loadbalancer for test

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/DistributedClusterTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/DistributedClusterTest.java
@@ -91,6 +91,7 @@ public class DistributedClusterTest extends KopProtocolHandlerTestBase {
         kConfig.setAllowAutoTopicCreation(true);
         kConfig.setAllowAutoTopicCreationType("partitioned");
         kConfig.setBrokerDeleteInactiveTopicsEnabled(false);
+        kConfig.setLoadBalancerEnabled(false);
 
         // set protocol related config
         URL testHandlerUrl = this.getClass().getClassLoader().getResource("test-protocol-handler.nar");
