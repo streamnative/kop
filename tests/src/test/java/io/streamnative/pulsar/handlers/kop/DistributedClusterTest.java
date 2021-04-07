e4d6e48d6a86cc4be56abf412e7f72af7cd9b7d9
hangc0276 <hangc0276@163.com>
2021-04-07 20:35:46 +0800

    format code

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/DistributedClusterTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/DistributedClusterTest.java
@@ -16,7 +16,6 @@ package io.streamnative.pulsar.handlers.kop;
 import static org.apache.kafka.common.internals.Topic.GROUP_METADATA_TOPIC_NAME;
 import static org.apache.pulsar.common.naming.TopicName.PARTITIONED_TOPIC_SUFFIX;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
