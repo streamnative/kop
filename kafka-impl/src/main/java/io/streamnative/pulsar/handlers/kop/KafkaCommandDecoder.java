streamnative/kop

commit a310c7e675ff6997b6f95ae7a31b6c6f7259ea16

Fix wrong semantics and usage of KafkaTopicManager#getTopic (#473)
### Motivation

Many usages of `KafkaTopicManager#getTopic` are wrong because they didn't check null value so that NPE may happen. And the semantics of `KafkaTopicManager#getTopic` is wrong because the `createIfMissing` argument of `BrokerService#getTopic` is true, so if a not existed topic's name was accepted as the argument, the topic would be created automatically. This behavior is unexpected because we only want to create topics automatically in `METADATA` request handler.

### Modifications
1. In `KafkaTopicManager#getTopic`, when `BrokerService#getTopic` is completed exceptionally, returns an exceptionally completed future instead of a null completed future.
2. Use `whenComplete` for all `getTopic` references so that we can handle null completed futures and exceptionally completed futures.
3. The `LIST_OFFSET` request handler is modified according to the above changes
    - For exceptionally completed `getTopic` future, return the thrown exception.
    - For null completed `getTopic` future, return the `UNKNOWN_TOPIC_OR_PARTITION` error.
4. Add tests for listing offsets of a not existed topic. Since the default `ctx` of `KafkaRequestHandler` is null and may cause NPE during error logging, we also set a mocked `ChannelHandlerContext` in the test.
5. Remove redundant NPE warning logs in `MessageFetchContext#handleFetch` since we've already added logs to the case that the future of `KafkaTopicConsumerManager` is completed exceptionally.

* Fix wrong semantics and usage of KafkaTopicManager#getTopic
* Use getOrCreateTopic for KafkaTopicManager#getTopicConsumerManager
* Differ the ServiceUnitNotReadyException from other exceptions
* Still use getTopic() for KafkaTopicManager#getTopicConsumerManager

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaTopicManager.java
@@ -127,22 +127,26 @@ public class KafkaTopicManager {
         return consumerTopicManagers.computeIfAbsent(
             topicName,
             t -> {
-                CompletableFuture<PersistentTopic> topic = getTopic(t);
-
-                return topic.thenApply(t2 -> {
-                    if (log.isDebugEnabled()) {
-                        log.debug("[{}] Call getTopicConsumerManager for {}, and create TCM for {}.",
-                            requestHandler.ctx.channel(), topicName, t2);
+                final CompletableFuture<KafkaTopicConsumerManager> tcmFuture = new CompletableFuture<>();
+                getTopic(t).whenComplete((persistentTopic, throwable) -> {
+                    if (persistentTopic != null && throwable == null) {
+                        if (log.isDebugEnabled()) {
+                            log.debug("[{}] Call getTopicConsumerManager for {}, and create TCM for {}.",
+                                    requestHandler.ctx.channel(), topicName, persistentTopic);
+                        }
+                        tcmFuture.complete(new KafkaTopicConsumerManager(requestHandler, persistentTopic));
+                    } else {
+                        if (throwable != null) {
+                            log.error("[{}] Failed to getTopicConsumerManager caused by getTopic '{}' throws {}",
+                                    requestHandler.ctx.channel(), topicName, throwable.getMessage());
+                        } else { // persistentTopic == null
+                            log.error("[{}] Failed to getTopicConsumerManager caused by getTopic '{}' returns empty",
+                                    requestHandler.ctx.channel(), topicName);
+                        }
+                        tcmFuture.complete(null);
                     }
-
-                    if (t2 == null) {
-                        // remove cache when topic is null
-                        removeTopicManagerCache(topic.toString());
-                        return null;
-                    }
-                    // return consumer manager
-                    return new KafkaTopicConsumerManager(requestHandler, t2);
                 });
+                return tcmFuture;
             }
         );
     }
@@ -229,10 +233,7 @@ public class KafkaTopicManager {
         }
     }
 
-    // For Produce/Consume we need to lookup, to make sure topic served by brokerService,
-    // or will meet error: "Service unit is not ready when loading the topic".
-    // If getTopic is called after lookup, then no needLookup.
-    // Returned Future wil complete with null when meet error.
+    // A wrapper of `BrokerService#getTopic` that is to find the topic's associated `PersistentTopic` instance
     public CompletableFuture<PersistentTopic> getTopic(String topicName) {
         if (closed.get()) {
             if (log.isDebugEnabled()) {
@@ -242,28 +243,28 @@ public class KafkaTopicManager {
             return CompletableFuture.completedFuture(null);
         }
         CompletableFuture<PersistentTopic> topicCompletableFuture = new CompletableFuture<>();
-        brokerService.getTopic(topicName, brokerService.isAllowAutoTopicCreation(topicName))
-                .whenComplete((t2, throwable) -> {
+        brokerService.getTopicIfExists(topicName).whenComplete((t2, throwable) -> {
             if (throwable != null) {
                 // The ServiceUnitNotReadyException is retriable so we should print a warning log instead of error log
                 if (throwable instanceof BrokerServiceException.ServiceUnitNotReadyException) {
                     log.warn("[{}] Failed to getTopic {}: {}",
                             requestHandler.ctx.channel(), topicName, throwable.getMessage());
+                    topicCompletableFuture.complete(null);
                 } else {
                     log.error("[{}] Failed to getTopic {}. exception:",
                             requestHandler.ctx.channel(), topicName, throwable);
+                    topicCompletableFuture.completeExceptionally(throwable);
                 }
                 // failed to getTopic from current broker, remove cache, which added in getTopicBroker.
                 removeTopicManagerCache(topicName);
-                topicCompletableFuture.complete(null);
                 return;
             }
             if (t2.isPresent()) {
                 PersistentTopic persistentTopic = (PersistentTopic) t2.get();
                 topicCompletableFuture.complete(persistentTopic);
             } else {
-                log.error("[{}]Get empty topic for name {}",
-                        requestHandler.ctx.channel(), topicName);
+                log.error("[{}]Get empty topic for name {}", requestHandler.ctx.channel(), topicName);
+                removeTopicManagerCache(topicName);
                 topicCompletableFuture.complete(null);
             }
         });

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/MessageFetchContext.java
@@ -124,21 +124,12 @@ public final class MessageFetchContext {
                 Map<TopicPartition, Pair<ManagedCursor, Long>> partitionCursor =
                     topicsAndCursor.entrySet().stream()
                         .map(pair -> {
-                            KafkaTopicConsumerManager tcm;
-                            try {
-                                // all future completed now.
-                                tcm = pair.getValue().get();
-                                if (tcm == null) {
-                                    // remove null future cache from consumerTopicManagers
-                                    KafkaTopicManager.getConsumerTopicManagers()
-                                            .remove(KopTopic.toString(pair.getKey()));
-                                    throw new NullPointerException("topic not owned, and return null TCM in fetch.");
-                                }
-                            } catch (Exception e) {
-                                log.warn("Error for get KafkaTopicConsumerManager.", e);
-
-                                responseData.put(pair.getKey(),
-                                    new FetchResponse.PartitionData(
+                            final TopicPartition topicPartition = pair.getKey();
+                            // The future is completed now
+                            final KafkaTopicConsumerManager tcm = pair.getValue().getNow(null);
+                            if (tcm == null) {
+                                // Current broker is not the owner broker of the partition
+                                responseData.put(topicPartition, new FetchResponse.PartitionData<>(
                                         Errors.NOT_LEADER_FOR_PARTITION,
                                         FetchResponse.INVALID_HIGHWATERMARK,
                                         FetchResponse.INVALID_LAST_STABLE_OFFSET,
@@ -150,21 +141,21 @@ public final class MessageFetchContext {
                             }
 
                             long offset = ((FetchRequest) fetchRequest.getRequest()).fetchData()
-                                .get(pair.getKey()).fetchOffset;
+                                .get(topicPartition).fetchOffset;
 
                             if (log.isDebugEnabled()) {
                                 log.debug("Fetch for {}: remove tcm to get cursor for fetch offset: {} .",
-                                    pair.getKey(), offset);
+                                    topicPartition, offset);
                             }
 
                             Pair<ManagedCursor, Long> cursorLongPair = tcm.remove(offset);
                             if (cursorLongPair == null) {
                                 log.warn("KafkaTopicConsumerManager.remove({}) return null for topic {}. "
                                         + "Fetch for topic return error.",
-                                    offset, pair.getKey());
+                                    offset, topicPartition);
 
-                                responseData.put(pair.getKey(),
-                                    new FetchResponse.PartitionData(
+                                responseData.put(topicPartition,
+                                    new FetchResponse.PartitionData<>(
                                         Errors.NOT_LEADER_FOR_PARTITION,
                                         FetchResponse.INVALID_HIGHWATERMARK,
                                         FetchResponse.INVALID_LAST_STABLE_OFFSET,
@@ -175,10 +166,10 @@ public final class MessageFetchContext {
                                 return null;
                             }
 
-                            highWaterMarkMap.put(pair.getKey(),
+                            highWaterMarkMap.put(topicPartition,
                                     MessageIdUtils.getHighWatermark(cursorLongPair.getLeft().getManagedLedger()));
 
-                            return Pair.of(pair.getKey(), cursorLongPair);
+                            return Pair.of(topicPartition, cursorLongPair);
                         })
                         .filter(x -> x != null)
                         .collect(Collectors.toMap(Pair::getKey, Pair::getValue));

tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaTopicConsumerManagerTest.java
@@ -23,12 +23,12 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.streamnative.pulsar.handlers.kop.coordinator.group.GroupCoordinator;
 import io.streamnative.pulsar.handlers.kop.coordinator.transaction.TransactionCoordinator;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
+
+import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.bookkeeper.mledger.ManagedCursor;
 import org.apache.bookkeeper.stats.NullStatsLogger;
@@ -40,6 +40,7 @@ import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.pulsar.broker.protocol.ProtocolHandler;
 import org.apache.pulsar.broker.service.persistent.PersistentTopic;
+import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.common.policies.data.TopicStats;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -53,7 +54,6 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
 
     private KafkaTopicManager kafkaTopicManager;
     private KafkaRequestHandler kafkaRequestHandler;
-    private SocketAddress serviceAddress;
     private AdminManager adminManager;
 
     @BeforeMethod
@@ -81,8 +81,6 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
         doReturn(mockChannel).when(mockCtx).channel();
         kafkaRequestHandler.ctx = mockCtx;
 
-        serviceAddress = new InetSocketAddress(pulsar.getBindAddress(), kafkaBrokerPort);
-
         kafkaTopicManager = new KafkaTopicManager(kafkaRequestHandler);
     }
 
@@ -93,10 +91,15 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
         super.internalCleanup();
     }
 
+    private void registerPartitionedTopic(final String topic) throws PulsarAdminException {
+        admin.topics().createPartitionedTopic(topic, 1);
+        pulsar.getBrokerService().getOrCreateTopic(topic);
+    }
+
     @Test
     public void testGetTopicConsumerManager() throws Exception {
         String topicName = "persistent://public/default/testGetTopicConsumerManager";
-        admin.lookups().lookupTopic(topicName);
+        registerPartitionedTopic(topicName);
         CompletableFuture<KafkaTopicConsumerManager> tcm = kafkaTopicManager.getTopicConsumerManager(topicName);
         KafkaTopicConsumerManager topicConsumerManager = tcm.get();
 
@@ -109,7 +112,7 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
 
         // 2. verify another get with different topic will return different tcm
         String topicName2 = "persistent://public/default/testGetTopicConsumerManager2";
-        admin.lookups().lookupTopic(topicName2);
+        registerPartitionedTopic(topicName2);
         tcm = kafkaTopicManager.getTopicConsumerManager(topicName2);
         topicConsumerManager2 = tcm.get();
         assertTrue(topicConsumerManager != topicConsumerManager2);
@@ -120,16 +123,16 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
     @Test
     public void testTopicConsumerManagerRemoveAndAdd() throws Exception {
         String topicName = "persistent://public/default/testTopicConsumerManagerRemoveAndAdd";
-        admin.lookups().lookupTopic(topicName);
+        registerPartitionedTopic(topicName);
 
         final Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + getKafkaBrokerPort());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
+        @Cleanup
         final KafkaProducer<Integer, String> producer = new KafkaProducer<>(props);
 
-        KProducer kProducer = new KProducer(topicName, true, getKafkaBrokerPort());
         int i = 0;
         String messagePrefix = "testTopicConsumerManagerRemoveAndAdd_message_";
         long offset = -1L;
@@ -198,6 +201,7 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
+        @Cleanup
         final KafkaProducer<Integer, String> producer = new KafkaProducer<>(props);
 
         int i = 0;

commit 0e3ef0de20f5e28c2a7adc7b6396f30f72c55920

Fix null KafkaTopicConsumerManager not removed (#495)
Fixes https://github.com/streamnative/kop/issues/493

This bug was introduced by https://github.com/streamnative/kop/pull/473. In `MessageFetchContext#handleFetch`, when the `KafkaTopicConsumerManager`'s future is completed with null, we should remove the future from `KafkaTopicManager#consumerTopicManagers`.

In addition, this PR adds some refactors for `consumerTopicManagers`:
1. Don't use getter to expose this field, use methods to operate it instead.
2. Check null for completed future before close `KafkaTopicConsumerManager`.

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaCommandDecoder.java
@@ -333,9 +333,9 @@ public abstract class KafkaCommandDecoder extends ChannelInboundHandlerAdapter {
             // case 4: responseFuture is completed normally
             responseFuture.thenApply(response -> {
                 if (log.isDebugEnabled()) {
-                    log.debug("Write kafka cmd responseAndRequest back to client. \n"
-                                    + "\trequest content: {} \n"
-                                    + "\tresponseAndRequest content: {}",
+                    log.debug("Write kafka cmd to client."
+                                    + " request content: {}"
+                                    + " responseAndRequest content: {}",
                             request, response.toString(request.getRequest().version()));
                 }
 

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaProtocolHandler.java
@@ -345,7 +345,7 @@ public class KafkaProtocolHandler implements ProtocolHandler {
         }
         KafkaTopicManager.LOOKUP_CACHE.clear();
         KopBrokerLookupManager.clear();
-        KafkaTopicManager.getConsumerTopicManagers().clear();
+        KafkaTopicManager.closeKafkaTopicConsumerManagers();
         KafkaTopicManager.getReferences().clear();
         KafkaTopicManager.getTopics().clear();
         OffsetAcker.CONSUMERS.clear();

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaTopicManager.java
@@ -15,6 +15,7 @@ package io.streamnative.pulsar.handlers.kop;
 
 import static com.google.common.base.Preconditions.checkState;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.streamnative.pulsar.handlers.kop.coordinator.group.OffsetAcker;
 import io.streamnative.pulsar.handlers.kop.utils.KopTopic;
 import java.net.InetSocketAddress;
@@ -23,8 +24,10 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -54,7 +57,6 @@ public class KafkaTopicManager {
     private final BrokerService brokerService;
 
     // consumerTopicManagers for consumers cache.
-    @Getter
     private static final ConcurrentHashMap<String, CompletableFuture<KafkaTopicConsumerManager>>
         consumerTopicManagers = new ConcurrentHashMap<>();
 
@@ -309,10 +311,7 @@ public class KafkaTopicManager {
         try {
             this.cursorExpireTask.cancel(true);
 
-            for (CompletableFuture<KafkaTopicConsumerManager> manager : consumerTopicManagers.values()) {
-                manager.get().close();
-            }
-            consumerTopicManagers.clear();
+            closeKafkaTopicConsumerManagers();
 
             for (Map.Entry<String, CompletableFuture<PersistentTopic>> entry : topics.entrySet()) {
                 String topicName = entry.getKey();
@@ -345,9 +344,14 @@ public class KafkaTopicManager {
         try {
             removeTopicManagerCache(topicName);
 
-            if (consumerTopicManagers.containsKey(topicName)) {
-                consumerTopicManagers.remove(topicName).get().close();
-            }
+            Optional.ofNullable(consumerTopicManagers.remove(topicName)).ifPresent(
+                    // Use thenAccept to avoid blocking
+                    tcmFuture -> tcmFuture.thenAccept(tcm -> {
+                        if (tcm != null) {
+                            tcm.close();
+                        }
+                    })
+            );
 
             if (!topics.containsKey(topicName)) {
                 return;
@@ -363,6 +367,27 @@ public class KafkaTopicManager {
         } catch (Exception e) {
             log.error("Failed to close reference for individual topic {}. exception:", topicName, e);
         }
+    }
+
+    public static void removeKafkaTopicConsumerManager(String topicName) {
+        consumerTopicManagers.remove(topicName);
+    }
+
+    public static void closeKafkaTopicConsumerManagers() {
+        consumerTopicManagers.forEach((topic, tcmFuture) -> {
+            try {
+                Optional.ofNullable(tcmFuture.get(300, TimeUnit.SECONDS))
+                        .ifPresent(KafkaTopicConsumerManager::close);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                log.warn("Failed to get TCM future of {} when trying to close it", topic);
+            }
+        });
+        consumerTopicManagers.clear();
+    }
+
+    @VisibleForTesting
+    public static int getNumberOfKafkaTopicConsumerManagers() {
+        return consumerTopicManagers.size();
     }
 
     public CompletableFuture<Consumer> getGroupConsumers(String groupId, TopicPartition kafkaPartition) {

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/MessageFetchContext.java
@@ -135,6 +135,8 @@ public final class MessageFetchContext {
                                         FetchResponse.INVALID_LOG_START_OFFSET,
                                         null,
                                         MemoryRecords.EMPTY));
+                                // remove null future cache from consumerTopicManagers
+                                KafkaTopicManager.removeKafkaTopicConsumerManager(KopTopic.toString(topicPartition));
                                 // result got. this will be filtered in following filter method.
                                 return null;
                             }
@@ -242,8 +244,8 @@ public final class MessageFetchContext {
                                                 "cursor.readEntry fail. deleteCursor");
                                     } else {
                                         // remove null future cache from consumerTopicManagers
-                                        KafkaTopicManager.getConsumerTopicManagers()
-                                                .remove(KopTopic.toString(kafkaTopic));
+                                        KafkaTopicManager.removeKafkaTopicConsumerManager(
+                                                KopTopic.toString(kafkaTopic));
                                         log.warn("Cursor deleted while TCM close.");
                                     }
                                 });
@@ -335,8 +337,8 @@ public final class MessageFetchContext {
                                     cm.add(pair.getRight(), pair);
                                 } else {
                                     // remove null future cache from consumerTopicManagers
-                                    KafkaTopicManager.getConsumerTopicManagers()
-                                        .remove(KopTopic.toString(kafkaPartition));
+                                    KafkaTopicManager.removeKafkaTopicConsumerManager(
+                                            KopTopic.toString(kafkaPartition));
                                     log.warn("Cursor deleted while TCM close, failed to add cursor back to TCM.");
                                 }
                             });

tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaTopicConsumerManagerTest.java
@@ -107,7 +107,7 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
         KafkaTopicConsumerManager topicConsumerManager2 = tcm.get();
 
         assertTrue(topicConsumerManager == topicConsumerManager2);
-        assertEquals(kafkaTopicManager.getConsumerTopicManagers().size(), 1);
+        assertEquals(KafkaTopicManager.getNumberOfKafkaTopicConsumerManagers(), 1);
 
         // 2. verify another get with different topic will return different tcm
         String topicName2 = "persistent://public/default/testGetTopicConsumerManager2";
@@ -115,7 +115,7 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
         tcm = kafkaTopicManager.getTopicConsumerManager(topicName2);
         topicConsumerManager2 = tcm.get();
         assertTrue(topicConsumerManager != topicConsumerManager2);
-        assertEquals(kafkaTopicManager.getConsumerTopicManagers().size(), 2);
+        assertEquals(KafkaTopicManager.getNumberOfKafkaTopicConsumerManagers(), 2);
     }
 
 