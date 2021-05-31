6b45943b57b7a842f7b384de8f1513ff8d924f5b
wuzhanpeng <wuzhanpeng@bigo.sg>
2021-05-31 20:25:56 +0800

    supplement a testcase(offset < logStartOffset)

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/OffsetResetTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/OffsetResetTest.java
@@ -16,16 +16,22 @@ package io.streamnative.pulsar.handlers.kop;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.mledger.ManagedLedgerConfig;
+import org.apache.bookkeeper.mledger.impl.ManagedLedgerImpl;
+import org.apache.bookkeeper.mledger.util.Futures;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.pulsar.broker.service.persistent.PersistentTopic;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -53,7 +59,7 @@ public class OffsetResetTest extends KopProtocolHandlerTestBase {
     }
 
     @Test(timeOut = 30000)
-    public void testResetOffset() throws Exception {
+    public void testGreaterThanEndOffset() throws Exception {
         final String topic = "persistent://public/default/test-reset-offset-topic";
         final String group = "test-reset-offset-groupid";
         final int numPartitions = 1;
@@ -137,6 +143,128 @@ public class OffsetResetTest extends KopProtocolHandlerTestBase {
         assertEquals(msgs, totalMsgs / 2);
         kConsumer.getConsumer().commitSync();
         log.info("finish re-consuming");
+
+        offset = adminClient.listConsumerGroupOffsets(group).partitionsToOffsetAndMetadata().get()
+                .get(topicPartition).offset();
+        leo = (long) kConsumer.getConsumer().endOffsets(Collections.singletonList(topicPartition))
+                .get(topicPartition);
+        log.info("offset:{}, leo:{}, lag:{}", offset, leo, leo - offset);
+
+        kProducer.close();
+        kConsumer.close();
+        adminClient.close();
+    }
+
+    @Test(timeOut = 30000)
+    public void testLessThanStartOffset() throws Exception {
+        final String topic = "persistent://public/default/test-reset-offset-topic";
+        final String group = "test-reset-offset-groupid";
+        final int numPartitions = 1;
+
+        // step1: create topic, produce some messages and consume to the end
+        admin.topics().createPartitionedTopic(topic, numPartitions);
+        TopicPartition topicPartition = new TopicPartition(topic, 0);
+
+        KProducer kProducer = new KProducer(topic, false, getKafkaBrokerPort());
+
+        int firstLedgerMsgs = 10;
+        String messageStrPrefix = topic + "_message_";
+
+        for (int i = 0; i < firstLedgerMsgs; i++) {
+            String messageStr = messageStrPrefix + i;
+            kProducer.getProducer().send(new ProducerRecord<>(topic, i, messageStr));
+        }
+        log.info("finish producing first ledger messages");
+
+        KConsumer kConsumer = new KConsumer(topic, getKafkaBrokerPort(), group);
+        kConsumer.getConsumer().subscribe(Collections.singleton(topic));
+        int msgs = 0;
+        while (msgs < firstLedgerMsgs) {
+            ConsumerRecords<Integer, String> records = kConsumer.getConsumer().poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<Integer, String> record : records) {
+                Integer key = record.key();
+                assertEquals(messageStrPrefix + key.toString(), record.value());
+                msgs++;
+            }
+        }
+        kConsumer.getConsumer().commitSync();
+        assertEquals(msgs, firstLedgerMsgs);
+        log.info("finish consuming first ledger messages");
+
+        Properties properties = new Properties();
+        properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + getKafkaBrokerPort());
+        AdminClient adminClient = AdminClient.create(properties);
+
+        long offset = adminClient.listConsumerGroupOffsets(group).partitionsToOffsetAndMetadata().get()
+                .get(topicPartition).offset();
+        long leo = (long) kConsumer.getConsumer().endOffsets(Collections.singletonList(topicPartition))
+                .get(topicPartition);
+        log.info("offset:{}, leo:{}, lag:{}", offset, leo, leo - offset);
+
+        // step2: force to trigger rollover and retention
+        // Note: pulsar format topic name is required
+        PersistentTopic persistentTopic = (PersistentTopic) pulsar.getBrokerService()
+                .getTopicIfExists(topic + "-partition-0").get().get();
+        ManagedLedgerImpl managedLedger = (ManagedLedgerImpl) persistentTopic.getManagedLedger();
+
+        ManagedLedgerConfig managedLedgerConfig = managedLedger.getConfig();
+        int maxEntriesPerLedger = managedLedgerConfig.getMaxEntriesPerLedger();
+        long minimumRolloverTime = managedLedgerConfig.getMinimumRolloverTimeMs();
+        long maximumRolloverTime = managedLedgerConfig.getMaximumRolloverTimeMs();
+        long retentionTime = managedLedgerConfig.getRetentionTimeMillis();
+
+        managedLedgerConfig.setMaxEntriesPerLedger(1);
+        managedLedgerConfig.setMinimumRolloverTime(0, TimeUnit.MILLISECONDS);
+        managedLedgerConfig.setMaximumRolloverTime(2, TimeUnit.MILLISECONDS);
+        managedLedgerConfig.setRetentionTime(2, TimeUnit.MILLISECONDS);
+        managedLedger.setConfig(managedLedgerConfig);
+
+        log.info("current ledger ids: {}", managedLedger.getLedgersInfo().keySet());
+
+        if (log.isDebugEnabled()) {
+            log.debug("minimumRolloverTimeMs:{}, maximumRolloverTimeMs:{}",
+                    managedLedgerConfig.getMinimumRolloverTimeMs(), managedLedgerConfig.getMaximumRolloverTimeMs());
+
+            Method currentLedgerIsFull = managedLedger.getClass().getDeclaredMethod("currentLedgerIsFull");
+            currentLedgerIsFull.setAccessible(true);
+            log.debug("Reflect rollCurrentLedgerIfFull:{}", currentLedgerIsFull.invoke(managedLedger));
+        }
+
+        managedLedger.rollCurrentLedgerIfFull();
+        managedLedger.trimConsumedLedgersInBackground(Futures.NULL_PROMISE);
+        Thread.sleep(1000);
+        log.info("current ledger ids: {}", managedLedger.getLedgersInfo().keySet());
+        log.info("finish deleting some ledgers");
+
+        // recovery configurations
+        managedLedgerConfig.setMaxEntriesPerLedger(maxEntriesPerLedger);
+        managedLedgerConfig.setMaximumRolloverTime((int) maximumRolloverTime, TimeUnit.MILLISECONDS);
+        managedLedgerConfig.setMinimumRolloverTime((int) minimumRolloverTime, TimeUnit.MILLISECONDS);
+        managedLedgerConfig.setRetentionTime((int) retentionTime, TimeUnit.MILLISECONDS);
+        managedLedger.setConfig(managedLedgerConfig);
+
+        // step3: produce second ledger messages
+        int secondLedgerMsgs = 5;
+        for (int i = firstLedgerMsgs; i < firstLedgerMsgs + secondLedgerMsgs; i++) {
+            String messageStr = messageStrPrefix + i;
+            kProducer.getProducer().send(new ProducerRecord<>(topic, i, messageStr));
+        }
+        log.info("finish producing second ledger messages");
+
+        // step4: consume second ledger messages from offset 0
+        kConsumer.getConsumer().seek(topicPartition, 0);
+        msgs = 0;
+        while (msgs < secondLedgerMsgs) {
+            ConsumerRecords<Integer, String> records = kConsumer.getConsumer().poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<Integer, String> record : records) {
+                Integer key = record.key();
+                assertEquals(messageStrPrefix + key.toString(), record.value());
+                log.info("2nd mesg: {}", record.value());
+                msgs++;
+            }
+        }
+        assertEquals(msgs, secondLedgerMsgs);
+        log.info("finish consuming second ledger messages");
 
         offset = adminClient.listConsumerGroupOffsets(group).partitionsToOffsetAndMetadata().get()
                 .get(topicPartition).offset();
