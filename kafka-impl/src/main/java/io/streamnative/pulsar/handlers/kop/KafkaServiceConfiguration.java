ee48f21614273c52e7b7e8a8141186bb7ca29e76
Yunze Xu <xyzinfernity@163.com>
2021-06-11 10:56:03 +0800

    Update documents for configuration (#558)
    Currently some configurations are not listed in docs. This PR adds these configurations and split them to different sections. BTW, fix the inconsistency between docs and code for `maxReadEntriesNum`. This PR also makes the partitions of metadata topics consistent with Kafka's config.

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaServiceConfiguration.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaServiceConfiguration.java
@@ -43,10 +43,10 @@ public class KafkaServiceConfiguration extends ServiceConfiguration {
     private static final int GroupInitialRebalanceDelayMs = 3000;
     // offset configuration
     private static final int OffsetsRetentionMinutes = 3 * 24 * 60;
-    public static final int DefaultOffsetsTopicNumPartitions = 8;
+    public static final int DefaultOffsetsTopicNumPartitions = 50;
     private static final int OffsetsMessageTTL = 3 * 24 * 3600;
     // txn configuration
-    public static final int DefaultTxnLogTopicNumPartitions = 8;
+    public static final int DefaultTxnLogTopicNumPartitions = 50;
 
     @Category
     private static final String CATEGORY_KOP = "Kafka on Pulsar";
@@ -293,7 +293,7 @@ public class KafkaServiceConfiguration extends ServiceConfiguration {
             category = CATEGORY_KOP,
             doc = "Maximum number of entries that are read from cursor once per time, default is 1"
     )
-    private int maxReadEntriesNum = 1;
+    private int maxReadEntriesNum = 5;
 
     @FieldContext(
             category = CATEGORY_KOP,
