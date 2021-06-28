c79f5703fc309011e1d30bf9a2bf8452625b87ae
mayozhang <mayozhang@tencent.com>
2021-06-28 11:33:06 +0800

    remove lagecy code

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/PulsarAuthEnabledTestBase.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/PulsarAuthEnabledTestBase.java
@@ -97,16 +97,6 @@ public abstract class PulsarAuthEnabledTestBase extends KopProtocolHandlerTestBa
         conf.setProperties(properties);
 
         super.internalSetup();
-        if (admin.tenants().getTenantInfo(TENANT) == null) {
-            admin.tenants().createTenant(TENANT,
-                    TenantInfo.builder()
-                            .adminRoles(Collections.singleton(ADMIN_USER))
-                            .allowedClusters(Collections.singleton(configClusterName))
-                            .build());
-        }
-        if (!admin.namespaces().getNamespaces(TENANT).contains(TENANT + "/" + NAMESPACE)) {
-            admin.namespaces().createNamespace(TENANT + "/" + NAMESPACE);
-        }
         admin.namespaces()
             .setNamespaceReplicationClusters(TENANT + "/" + NAMESPACE, Sets.newHashSet(super.configClusterName));
         admin.topics().createPartitionedTopic(TOPIC, 1);
