ace004aedefaed300897ba1058c17bce0bc05c98
wenbingshen <oliver.shen999@gmail.com>
2021-08-16 15:08:25 +0800

    Simplify the MessageFetchContextTest

--- kafka-impl/src/test/java/io/streamnative/pulsar/handlers/kop/MessageFetchContextTest.java
+++ kafka-impl/src/test/java/io/streamnative/pulsar/handlers/kop/MessageFetchContextTest.java
@@ -13,36 +13,24 @@
  */
 package io.streamnative.pulsar.handlers.kop;
 
-import static org.apache.kafka.common.protocol.CommonFields.THROTTLE_TIME_MS;
 import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
 
-import io.streamnative.pulsar.handlers.kop.format.DecodeResult;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.protocol.Errors;
-import org.apache.kafka.common.record.MemoryRecords;
 import org.apache.kafka.common.requests.AbstractResponse;
 import org.apache.kafka.common.requests.FetchRequest;
-import org.apache.kafka.common.requests.FetchResponse;
 import org.apache.kafka.common.requests.ResponseCallbackWrapper;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.collections.Maps;
 
 public class MessageFetchContextTest {
-
-    private static final Map<TopicPartition, FetchResponse.PartitionData<MemoryRecords>>
-            responseData = new ConcurrentHashMap();
 
     private static final Map<TopicPartition, FetchRequest.PartitionData> fetchData = Maps.newHashMap();
 
@@ -51,17 +39,9 @@ public class MessageFetchContextTest {
     FetchRequest fetchRequest = FetchRequest.Builder
             .forConsumer(0, 0, fetchData).build();
 
-    private static final ConcurrentLinkedQueue<DecodeResult> decodeResults = new ConcurrentLinkedQueue<>();
-
     private static final String topicName = "test-fetch";
     private static final TopicPartition tp1 = new TopicPartition(topicName, 1);
     private static final TopicPartition tp2 = new TopicPartition(topicName, 2);
-
-    // Competitive problems need a certain chance to appear.
-    // Try a few more times to have a higher chance of reproducing the problem.
-    private static final int totalAttempts = 200;
-
-    private static final AtomicInteger currentAttempts = new AtomicInteger(0);
 
     private static volatile MessageFetchContext messageFetchContext = null;
 
@@ -73,29 +53,17 @@ public class MessageFetchContextTest {
         messageFetchContext = MessageFetchContext.getForTest(fetchRequest, resultFuture);
     }
 
-    private void startThreads(Boolean isSafe) throws Exception {
+    private void startThreads() throws Exception {
         Thread run1 = new Thread(() -> {
-            if (isSafe) {
-                // For the synchronous method, we can check it only once,
-                // because if there is still has problem, it will eventually become a flaky test,
-                // If it does not become a flaky test, then we can keep this
-                messageFetchContext.addErrorPartitionResponseForTest(tp1, Errors.NONE);
-            } else {
-                for (int i = 0; i < 100; i++) {
-                    addErrorPartitionResponse(tp1, Errors.NONE);
-                }
-            }
+            // For the synchronous method, we can check it only once,
+            // because if there is still has problem, it will eventually become a flaky test,
+            // If it does not become a flaky test, then we can keep this
+            messageFetchContext.addErrorPartitionResponseForTest(tp1, Errors.NONE);
         });
 
         Thread run2 = new Thread(() -> {
-            if (isSafe) {
-                // As comment described in run1, we can check it only once.
-                messageFetchContext.addErrorPartitionResponseForTest(tp2, Errors.NONE);
-            } else {
-                for (int i = 0; i < 100; i++) {
-                    addErrorPartitionResponse(tp2, Errors.NONE);
-                }
-            }
+            // As comment described in run1, we can check it only once.
+            messageFetchContext.addErrorPartitionResponseForTest(tp2, Errors.NONE);
         });
 
         run1.start();
@@ -104,7 +72,7 @@ public class MessageFetchContextTest {
         run2.join();
     }
 
-    private void startAndGetResult(Boolean isSafe, AtomicReference<Set<Errors>> errorsSet, Boolean isBlock)
+    private void startAndGetResult(AtomicReference<Set<Errors>> errorsSet)
             throws Exception {
 
         BiConsumer<AbstractResponse, Throwable> action = (response, exception) -> {
@@ -114,24 +82,8 @@ public class MessageFetchContextTest {
             errorsSet.set(currentErrors);
         };
 
-        while (currentAttempts.getAndIncrement() <= totalAttempts || isBlock) {
-            startThreads(isSafe);
-            resultFuture.whenComplete(action);
-            resultFuture = new CompletableFuture<>();
-            responseData.clear();
-            if ((isBlock && errorsSet.get().contains(Errors.REQUEST_TIMED_OUT)) || isSafe) {
-                break;
-            }
-        }
-    }
-
-    // Since the code has been changed to a synchronized state,
-    // competitive problem is reproduced by simulating the code before the modification
-    @Test
-    public void testHandleFetchUnSafe() throws Exception {
-        AtomicReference<Set<Errors>> errorsSet = new AtomicReference<>(new HashSet<>());
-        startAndGetResult(false, errorsSet, true);
-        assertTrue(errorsSet.get().contains(Errors.REQUEST_TIMED_OUT));
+        startThreads();
+        resultFuture.whenComplete(action);
     }
 
     // Run the actually modified code logic in MessageFetchContext
@@ -141,76 +93,8 @@ public class MessageFetchContextTest {
     @Test
     public void testHandleFetchSafe() throws Exception {
         AtomicReference<Set<Errors>> errorsSet = new AtomicReference<>(new HashSet<>());
-        startAndGetResult(true, errorsSet, false);
+        startAndGetResult(errorsSet);
         assertFalse(errorsSet.get().contains(Errors.REQUEST_TIMED_OUT));
     }
 
-    private void addErrorPartitionResponse(TopicPartition topicPartition, Errors errors) {
-        responseData.put(topicPartition, new FetchResponse.PartitionData<>(
-                errors,
-                FetchResponse.INVALID_HIGHWATERMARK,
-                FetchResponse.INVALID_LAST_STABLE_OFFSET,
-                FetchResponse.INVALID_LOG_START_OFFSET,
-                null,
-                MemoryRecords.EMPTY));
-
-        tryComplete();
-    }
-
-    private void tryComplete() {
-        if (responseData.size() >= fetchRequest.fetchData().size()) {
-            complete();
-        }
-    }
-
-    private void complete() {
-        if (resultFuture == null) {
-            // the context has been recycled
-            return;
-        }
-        if (resultFuture.isCancelled()) {
-            // The request was cancelled by KafkaCommandDecoder when channel is closed or this request is expired,
-            // so the Netty buffers should be released.
-            decodeResults.forEach(DecodeResult::release);
-            return;
-        }
-        if (resultFuture.isDone()) {
-            // It may be triggered again in DelayedProduceAndFetch
-            return;
-        }
-
-        // Keep the order of TopicPartition
-        final LinkedHashMap<TopicPartition, FetchResponse.PartitionData<MemoryRecords>>
-                orderedResponseData = new LinkedHashMap<>();
-        // add the topicPartition with timeout error if it's not existed in responseData
-        fetchRequest.fetchData().keySet().forEach(topicPartition -> {
-            final FetchResponse.PartitionData<MemoryRecords> partitionData = responseData.remove(topicPartition);
-            if (partitionData != null) {
-                orderedResponseData.put(topicPartition, partitionData);
-            } else {
-                orderedResponseData.put(topicPartition, new FetchResponse.PartitionData<>(
-                        Errors.REQUEST_TIMED_OUT,
-                        FetchResponse.INVALID_HIGHWATERMARK,
-                        FetchResponse.INVALID_LAST_STABLE_OFFSET,
-                        FetchResponse.INVALID_LOG_START_OFFSET,
-                        null,
-                        MemoryRecords.EMPTY));
-            }
-        });
-
-        // Create another reference to this.decodeResults so the lambda expression will capture this local reference
-        // because this.decodeResults will be reset to null after resultFuture is completed.
-        final ConcurrentLinkedQueue<DecodeResult> decodeResults = MessageFetchContextTest.decodeResults;
-        resultFuture.complete(
-                new ResponseCallbackWrapper(
-                        new FetchResponse<>(
-                                Errors.NONE,
-                                orderedResponseData,
-                                ((Integer) THROTTLE_TIME_MS.defaultValue),
-                                fetchRequest.metadata().sessionId()),
-                        () -> {
-                            // release the batched ByteBuf if necessary
-                            decodeResults.forEach(DecodeResult::release);
-                        }));
-    }
 }
