ace95376dd3d3791c362608138b0e6a5e87e63fa
Kai Wang <kwang@streamnative.io>
2021-08-17 13:42:59 +0800

    [FEATURE] Add Authorizer used for authorization. (#657)
    ## Motivation
    #236

---
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/security/KafkaPrincipal.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop.security;
+
+
+import java.security.Principal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+
+/**
+ * Store client login info.
+ */
+@Getter
+@ToString
+@AllArgsConstructor
+public class KafkaPrincipal implements Principal {
+
+    public static final String USER_TYPE = "User";
+
+    private final String principalType;
+
+    /**
+     * Pulsar role.
+     */
+    private final String name;
+
+}
