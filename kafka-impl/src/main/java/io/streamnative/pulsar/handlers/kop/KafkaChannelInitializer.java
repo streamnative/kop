streamnative/kop

commit a50af461d6c3787031d8bf3be768024eab1de5d1

Fix publish throttling and LoadManager API for pulsar upgrade (#488)
This PR upgrades pulsar dependency to 2.8.0-rc-202105092228, which has two major API changes.

https://github.com/apache/pulsar/pull/10391 changed `LoadManager` API so that `MetadataCache` is used instead of `ZookeeperCache` in this PR.

https://github.com/apache/pulsar/pull/7406 changed the throttling strategy. However, currently KoP is different from Pulsar that the produce and its callback may be in different threads. KoP calls `PersistentTopic#publishMessages` in a callback of `KafkaTopicManager#getTopic` if the returned future is not completed immediately. Otherwise, it's called just in the I/O thread. Therefore, here we still use a **channel based** publish bytes stats for throttling, while https://github.com/apache/pulsar/pull/7406 uses a **thread based** publish bytes stats.

The other refactors are:
1. Change the throttling related fields from `InternalServerCnx` to `KafkaRequestHandler`.
2. Use `BrokerService#getPausedConnections` to check if the channel's auto read is disabled and modify the tests as well.



* Fix LoadManager interface
* Refactor publish throttling
* Remove ZookeeperCache usage

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -59,6 +59,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -157,9 +158,9 @@ import org.apache.pulsar.common.protocol.Commands;
 import org.apache.pulsar.common.schema.KeyValue;
 import org.apache.pulsar.common.util.FutureUtil;
 import org.apache.pulsar.common.util.Murmur3_32Hash;
+import org.apache.pulsar.metadata.api.MetadataCache;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 import org.apache.pulsar.policies.data.loadbalancer.ServiceLookupData;
-import org.apache.pulsar.zookeeper.ZooKeeperCache;
-import org.apache.pulsar.zookeeper.ZooKeeperCache.Deserializer;
 
 /**
  * This class contains all the request handling methods.
@@ -196,6 +197,12 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
     // is found.
     private final Map<TopicPartition, PendingTopicFutures> pendingTopicFuturesMap = new ConcurrentHashMap<>();
 
+    // Flag to manage throttling-publish-buffer by atomically enable/disable read-channel.
+    private final long maxPendingBytes;
+    private final long resumeThresholdPendingBytes;
+    private final AtomicLong pendingBytes = new AtomicLong(0);
+    private volatile boolean autoReadDisabledPublishBufferLimiting = false;
+
     public KafkaRequestHandler(PulsarService pulsarService,
                                KafkaServiceConfiguration kafkaConfig,
                                GroupCoordinator groupCoordinator,
@@ -226,6 +233,8 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         this.entryFormatter = EntryFormatterFactory.create(kafkaConfig.getEntryFormat());
         this.currentConnectedGroup = new ConcurrentHashMap<>();
         this.groupIdStoredPath = kafkaConfig.getGroupIdZooKeeperPath();
+        this.maxPendingBytes = kafkaConfig.getMaxMessagePublishBufferSizeInMB() * 1024L * 1024L;
+        this.resumeThresholdPendingBytes = this.maxPendingBytes / 2;
     }
 
     @Override
@@ -244,7 +253,6 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         log.info("channel inactive {}", ctx.channel());
 
         close();
-        isActive.set(false);
     }
 
     @Override
@@ -627,6 +635,54 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         });
     }
 
+    private void disableCnxAutoRead() {
+        if (ctx != null && ctx.channel().config().isAutoRead()) {
+            ctx.channel().config().setAutoRead(false);
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] disable auto read", ctx.channel());
+            }
+        }
+    }
+
+    private void enableCnxAutoRead() {
+        if (ctx != null && !ctx.channel().config().isAutoRead()
+                && !autoReadDisabledPublishBufferLimiting) {
+            // Resume reading from socket if pending-request is not reached to threshold
+            ctx.channel().config().setAutoRead(true);
+            // triggers channel read
+            ctx.read();
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] enable auto read", ctx.channel());
+            }
+        }
+    }
+
+    private void startSendOperationForThrottling(long msgSize) {
+        final long currentPendingBytes = pendingBytes.addAndGet(msgSize);
+        if (currentPendingBytes >= maxPendingBytes && !autoReadDisabledPublishBufferLimiting && maxPendingBytes > 0) {
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] disable auto read because currentPendingBytes({}) > maxPendingBytes({})",
+                        ctx.channel(), currentPendingBytes, maxPendingBytes);
+            }
+            disableCnxAutoRead();
+            autoReadDisabledPublishBufferLimiting = true;
+            pulsarService.getBrokerService().pausedConnections(1);
+        }
+    }
+
+    private void completeSendOperationForThrottling(long msgSize) {
+        final long currentPendingBytes = pendingBytes.addAndGet(-msgSize);
+        if (currentPendingBytes < resumeThresholdPendingBytes && autoReadDisabledPublishBufferLimiting) {
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] enable auto read because currentPendingBytes({}) < resumeThreshold({})",
+                        ctx.channel(), currentPendingBytes, resumeThresholdPendingBytes);
+            }
+            autoReadDisabledPublishBufferLimiting = false;
+            enableCnxAutoRead();
+            pulsarService.getBrokerService().resumedConnections(1);
+        }
+    }
+
     private void publishMessages(final PersistentTopic persistentTopic,
                                  final ByteBuf byteBuf,
                                  final int numMessages,
@@ -654,9 +710,10 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         final long beforePublish = MathUtils.nowInNano();
         persistentTopic.publishMessage(byteBuf,
                 MessagePublishContext.get(offsetFuture, persistentTopic, numMessages, System.nanoTime()));
-        byteBuf.release();
         final RecordBatch batch = records.batchIterator().next();
         offsetFuture.whenComplete((offset, e) -> {
+            completeSendOperationForThrottling(byteBuf.readableBytes());
+            byteBuf.release();
             if (e == null) {
                 if (batch.isTransactional()) {
                     transactionCoordinator.addActivePidOffset(TopicName.get(partitionName), batch.producerId(), offset);
@@ -681,9 +738,6 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         ProduceRequest produceRequest = (ProduceRequest) produceHar.getRequest();
 
         final int numPartitions = produceRequest.partitionRecordsOrFail().size();
-
-        final long dataSizePerPartition = produceHar.getBuffer().readableBytes();
-        topicManager.getInternalServerCnx().increasePublishBuffer(dataSizePerPartition);
 
         final Map<TopicPartition, PartitionResponse> responseMap = new ConcurrentHashMap<>();
         final CompletableFuture<Void> produceFuture = new CompletableFuture<>();
@@ -718,6 +772,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                 final ByteBuf byteBuf = entryFormatter.encode(validRecords, numMessages);
                 requestStats.getProduceEncodeStats().registerSuccessfulEvent(
                         MathUtils.elapsedNanos(beforeRecordsProcess), TimeUnit.NANOSECONDS);
+                startSendOperationForThrottling(byteBuf.readableBytes());
 
                 if (log.isDebugEnabled()) {
                     log.debug("[{}] Request {}: Produce messages for topic {} partition {}, request size: {} ",
@@ -756,7 +811,6 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         });
 
         produceFuture.thenApply(ignored -> {
-            topicManager.getInternalServerCnx().decreasePublishBuffer(dataSizePerPartition);
             if (log.isDebugEnabled()) {
                 log.debug("[{}] Request {}: Complete handle produce.", ctx.channel(), produceHar.toString());
             }
@@ -1704,8 +1758,9 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         }
         // advertised data is write in  /loadbalance/brokers/advertisedAddress:webServicePort
         // here we get the broker url, need to find related webServiceUrl.
-        ZooKeeperCache zkCache = pulsarService.getLocalZkCache();
-        zkCache.getChildrenAsync(LoadManager.LOADBALANCE_BROKERS_ROOT, zkCache)
+        pulsarService.getPulsarResources()
+            .getDynamicConfigResources()
+            .getChildrenAsync(LoadManager.LOADBALANCE_BROKERS_ROOT)
             .whenComplete((set, throwable) -> {
                 if (throwable != null) {
                     log.error("Error in getChildrenAsync(zk://loadbalance) for {}", pulsarAddress, throwable);
@@ -1730,12 +1785,11 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                 }
 
                 // Get a list of ServiceLookupData for each matchBroker.
-                List<CompletableFuture<Optional<ServiceLookupData>>> list = matchBrokers.stream()
-                    .map(matchBroker ->
-                        zkCache.getDataAsync(
-                            String.format("%s/%s", LoadManager.LOADBALANCE_BROKERS_ROOT, matchBroker),
-                            (Deserializer<ServiceLookupData>)
-                                pulsarService.getLoadManager().get().getLoadReportDeserializer()))
+                final MetadataCache<LocalBrokerData> metadataCache = pulsarService.getLocalMetadataStore()
+                        .getMetadataCache(LocalBrokerData.class);
+                List<CompletableFuture<Optional<LocalBrokerData>>> list = matchBrokers.stream()
+                    .map(matchBroker -> metadataCache.get(
+                            String.format("%s/%s", LoadManager.LOADBALANCE_BROKERS_ROOT, matchBroker)))
                     .collect(Collectors.toList());
 
                 FutureUtil.waitForAll(list)
@@ -1748,7 +1802,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                             }
 
                             try {
-                                for (CompletableFuture<Optional<ServiceLookupData>> lookupData : list) {
+                                for (CompletableFuture<Optional<LocalBrokerData>> lookupData : list) {
                                     ServiceLookupData data = lookupData.get().get();
                                     if (log.isDebugEnabled()) {
                                         log.debug("Handle getProtocolDataToAdvertise for {}, pulsarUrl: {}, "

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/InternalServerCnx.java
@@ -13,9 +13,7 @@
  */
 package io.streamnative.pulsar.handlers.kop;
 
-import com.google.common.annotations.VisibleForTesting;
 import java.net.InetSocketAddress;
-import java.util.concurrent.atomic.AtomicLongFieldUpdater;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.broker.service.Producer;
@@ -31,10 +29,6 @@ import org.apache.pulsar.broker.service.ServerCnx;
 public class InternalServerCnx extends ServerCnx {
     @Getter
     KafkaRequestHandler kafkaRequestHandler;
-
-    private static final AtomicLongFieldUpdater<InternalServerCnx> KOP_MSG_PUBLISH_BUFFER_SIZE_UPDATER =
-            AtomicLongFieldUpdater.newUpdater(InternalServerCnx.class, "kopMessagePublishBufferSize");
-    private volatile long kopMessagePublishBufferSize = 0;
 
     public InternalServerCnx(KafkaRequestHandler kafkaRequestHandler) {
         super(kafkaRequestHandler.getPulsarService());
@@ -63,54 +57,21 @@ public class InternalServerCnx extends ServerCnx {
 
     // called after channel active
     public void updateCtx() {
-        this.remoteAddress = kafkaRequestHandler.getRemoteAddress();
+        this.remoteAddress = kafkaRequestHandler.remoteAddress;
     }
 
     @Override
     public void enableCnxAutoRead() {
-        if (!kafkaRequestHandler.ctx.channel().config().isAutoRead()) {
-            kafkaRequestHandler.ctx.channel().config().setAutoRead(true);
-            kafkaRequestHandler.ctx.read();
-            if (log.isDebugEnabled()) {
-                log.debug("Channel {}  auto read has set to true.", kafkaRequestHandler.ctx.channel());
-            }
-        }
+        // do nothing is this mock
     }
 
     @Override
     public void disableCnxAutoRead() {
-        if (kafkaRequestHandler.ctx.channel().config().isAutoRead()) {
-            kafkaRequestHandler.ctx.channel().config().setAutoRead(false);
-            if (log.isDebugEnabled()) {
-                log.debug("Channel {} auto read has set to false.", kafkaRequestHandler.ctx.channel());
-            }
-        }
-    }
-
-    public void increasePublishBuffer(long msgSize) {
-        KOP_MSG_PUBLISH_BUFFER_SIZE_UPDATER.getAndAdd(this, msgSize);
-        if (getBrokerService().isReachMessagePublishBufferThreshold()) {
-            disableCnxAutoRead();
-        }
-    }
-
-    public void decreasePublishBuffer(long msgSize) {
-        KOP_MSG_PUBLISH_BUFFER_SIZE_UPDATER.getAndAdd(this, -msgSize);
+        // do nothing is this mock
     }
 
     @Override
-    public long getMessagePublishBufferSize() {
-        return kopMessagePublishBufferSize;
-    }
-
-
     public void cancelPublishBufferLimiting() {
-        // do nothing.
+        // do nothing is this mock
     }
-
-    @VisibleForTesting
-    public void setMessagePublishBufferSize(long size) {
-        this.kopMessagePublishBufferSize = size;
-    }
-
 }

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KopBrokerLookupManager.java
@@ -31,6 +31,8 @@ import org.apache.pulsar.broker.service.persistent.PersistentTopic;
 import org.apache.pulsar.client.impl.PulsarClientImpl;
 import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.util.FutureUtil;
+import org.apache.pulsar.metadata.api.MetadataCache;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 import org.apache.pulsar.policies.data.loadbalancer.ServiceLookupData;
 import org.apache.pulsar.zookeeper.ZooKeeperCache;
 
@@ -211,12 +213,11 @@ public class KopBrokerLookupManager {
                     }
 
                     // Get a list of ServiceLookupData for each matchBroker.
-                    List<CompletableFuture<Optional<ServiceLookupData>>> list = matchBrokers.stream()
-                            .map(matchBroker ->
-                                zkCache.getDataAsync(
-                                    String.format("%s/%s", LoadManager.LOADBALANCE_BROKERS_ROOT, matchBroker),
-                                    (ZooKeeperCache.Deserializer<ServiceLookupData>)
-                                            pulsarService.getLoadManager().get().getLoadReportDeserializer()))
+                    final MetadataCache<LocalBrokerData> metadataCache = pulsarService.getLocalMetadataStore()
+                            .getMetadataCache(LocalBrokerData.class);
+                    List<CompletableFuture<Optional<LocalBrokerData>>> list = matchBrokers.stream()
+                            .map(matchBroker -> metadataCache.get(
+                                    String.format("%s/%s", LoadManager.LOADBALANCE_BROKERS_ROOT, matchBroker)))
                             .collect(Collectors.toList());
 
                     getKopAddress(list, pulsarAddress, kopAddressFuture, topic, hostAndPort);
@@ -224,7 +225,7 @@ public class KopBrokerLookupManager {
         return kopAddressFuture;
     }
 
-    private void getKopAddress(List<CompletableFuture<Optional<ServiceLookupData>>> list,
+    private void getKopAddress(List<CompletableFuture<Optional<LocalBrokerData>>> list,
                                InetSocketAddress pulsarAddress,
                                CompletableFuture<Optional<String>> kopAddressFuture,
                                TopicName topic,
@@ -239,7 +240,7 @@ public class KopBrokerLookupManager {
                     }
 
                     try {
-                        for (CompletableFuture<Optional<ServiceLookupData>> lookupData : list) {
+                        for (CompletableFuture<Optional<LocalBrokerData>> lookupData : list) {
                             ServiceLookupData data = lookupData.get().get();
                             if (log.isDebugEnabled()) {
                                 log.debug("Handle getProtocolDataToAdvertise for {}, pulsarUrl: {}, "

commit 6faba390dc65fa3c5976762fd0185ac1badc680c

Avoid creating a lot of MetadataCache instances (#663)
### Motivation

When there're a lot of topics, there could be a lot of `MetadataCache` instances in memory.

![image](https://user-images.githubusercontent.com/18204803/129903767-4b2f3961-c0e1-4ad5-88b0-7451ed41393a.png)

This bug was introduced from https://github.com/streamnative/kop/pull/488 because the Pulsar side adopted a new class `MetadataCache` as ZK cache.  However, each time `MetadataStore#getMetadataCache` is called, a new `MetadataCache` instance will be created. In KoP, it means that each time a topic lookup is performed, a new `MetadataCache` instance will be created.

### Modifications

Only call `MetadataStore#getMetadataCache` once when KoP starts and reuse the same `MetadataCache` instance for each `KafkaRequestHandler` instance.

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaChannelInitializer.java
@@ -26,6 +26,8 @@ import io.streamnative.pulsar.handlers.kop.stats.StatsLogger;
 import io.streamnative.pulsar.handlers.kop.utils.ssl.SSLUtils;
 import lombok.Getter;
 import org.apache.pulsar.broker.PulsarService;
+import org.apache.pulsar.metadata.api.MetadataCache;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 /**
@@ -52,6 +54,7 @@ public class KafkaChannelInitializer extends ChannelInitializer<SocketChannel> {
     private final SslContextFactory.Server sslContextFactory;
     @Getter
     private final StatsLogger statsLogger;
+    private final MetadataCache<LocalBrokerData> localBrokerDataCache;
 
     public KafkaChannelInitializer(PulsarService pulsarService,
                                    KafkaServiceConfiguration kafkaConfig,
@@ -60,7 +63,8 @@ public class KafkaChannelInitializer extends ChannelInitializer<SocketChannel> {
                                    AdminManager adminManager,
                                    boolean enableTLS,
                                    EndPoint advertisedEndPoint,
-                                   StatsLogger statsLogger) {
+                                   StatsLogger statsLogger,
+                                   MetadataCache<LocalBrokerData> localBrokerDataCache) {
         super();
         this.pulsarService = pulsarService;
         this.kafkaConfig = kafkaConfig;
@@ -70,6 +74,7 @@ public class KafkaChannelInitializer extends ChannelInitializer<SocketChannel> {
         this.enableTls = enableTLS;
         this.advertisedEndPoint = advertisedEndPoint;
         this.statsLogger = statsLogger;
+        this.localBrokerDataCache = localBrokerDataCache;
 
         if (enableTls) {
             sslContextFactory = SSLUtils.createSslContextFactory(kafkaConfig);
@@ -88,7 +93,7 @@ public class KafkaChannelInitializer extends ChannelInitializer<SocketChannel> {
             new LengthFieldBasedFrameDecoder(MAX_FRAME_LENGTH, 0, 4, 0, 4));
         ch.pipeline().addLast("handler",
             new KafkaRequestHandler(pulsarService, kafkaConfig,
-                    groupCoordinator, transactionCoordinator, adminManager,
+                    groupCoordinator, transactionCoordinator, adminManager, localBrokerDataCache,
                     enableTls, advertisedEndPoint, statsLogger));
     }
 

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaProtocolHandler.java
@@ -68,6 +68,8 @@ import org.apache.pulsar.common.naming.NamespaceName;
 import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.policies.data.ClusterData;
 import org.apache.pulsar.common.util.FutureUtil;
+import org.apache.pulsar.metadata.api.MetadataCache;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 
 /**
  * Kafka Protocol Handler load and run by Pulsar Service.
@@ -83,6 +85,7 @@ public class KafkaProtocolHandler implements ProtocolHandler {
     private PrometheusMetricsProvider statsProvider;
     private KopBrokerLookupManager kopBrokerLookupManager;
     private AdminManager adminManager = null;
+    private MetadataCache<LocalBrokerData> localBrokerDataCache;
 
     @Getter
     private KafkaServiceConfiguration kafkaConfig;
@@ -259,6 +262,10 @@ public class KafkaProtocolHandler implements ProtocolHandler {
             KopVersion.getBuildHost(),
             KopVersion.getBuildTime());
 
+        // Currently each time getMetadataCache() is called, a new MetadataCache<T> instance will be created, even for
+        // the same type. So we must reuse the same MetadataCache<LocalBrokerData> to avoid creating a lot of instances.
+        localBrokerDataCache = brokerService.pulsar().getLocalMetadataStore().getMetadataCache(LocalBrokerData.class);
+
         ZooKeeperUtils.tryCreatePath(brokerService.pulsar().getZkClient(),
                 kafkaConfig.getGroupIdZooKeeperPath(), new byte[0]);
 
@@ -353,13 +360,13 @@ public class KafkaProtocolHandler implements ProtocolHandler {
                     case SASL_PLAINTEXT:
                         builder.put(endPoint.getInetAddress(), new KafkaChannelInitializer(brokerService.getPulsar(),
                                 kafkaConfig, groupCoordinator, transactionCoordinator, adminManager, false,
-                            advertisedEndPoint, rootStatsLogger.scope(SERVER_SCOPE)));
+                            advertisedEndPoint, rootStatsLogger.scope(SERVER_SCOPE), localBrokerDataCache));
                         break;
                     case SSL:
                     case SASL_SSL:
                         builder.put(endPoint.getInetAddress(), new KafkaChannelInitializer(brokerService.getPulsar(),
                                 kafkaConfig, groupCoordinator, transactionCoordinator, adminManager, true,
-                            advertisedEndPoint, rootStatsLogger.scope(SERVER_SCOPE)));
+                            advertisedEndPoint, rootStatsLogger.scope(SERVER_SCOPE), localBrokerDataCache));
                         break;
                 }
             });

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -194,6 +194,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
     private final SaslAuthenticator authenticator;
     private final Authorizer authorizer;
     private final AdminManager adminManager;
+    private final MetadataCache<LocalBrokerData> localBrokerDataCache;
 
     private final Boolean tlsEnabled;
     private final EndPoint advertisedEndPoint;
@@ -236,6 +237,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                                GroupCoordinator groupCoordinator,
                                TransactionCoordinator transactionCoordinator,
                                AdminManager adminManager,
+                               MetadataCache<LocalBrokerData> localBrokerDataCache,
                                Boolean tlsEnabled,
                                EndPoint advertisedEndPoint,
                                StatsLogger statsLogger) throws Exception {
@@ -256,6 +258,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                 ? new SimpleAclAuthorizer(pulsarService)
                 : null;
         this.adminManager = adminManager;
+        this.localBrokerDataCache = localBrokerDataCache;
         this.tlsEnabled = tlsEnabled;
         this.advertisedEndPoint = advertisedEndPoint;
         this.advertisedListeners = kafkaConfig.getKafkaAdvertisedListeners();
@@ -1970,10 +1973,8 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                 }
 
                 // Get a list of ServiceLookupData for each matchBroker.
-                final MetadataCache<LocalBrokerData> metadataCache = pulsarService.getLocalMetadataStore()
-                        .getMetadataCache(LocalBrokerData.class);
                 List<CompletableFuture<Optional<LocalBrokerData>>> list = matchBrokers.stream()
-                    .map(matchBroker -> metadataCache.get(
+                    .map(matchBroker -> localBrokerDataCache.get(
                             String.format("%s/%s", LoadManager.LOADBALANCE_BROKERS_ROOT, matchBroker)))
                     .collect(Collectors.toList());
 

tests/src/test/java/io/streamnative/pulsar/handlers/kop/EntryPublishTimeTest.java
@@ -24,6 +24,7 @@ import io.streamnative.pulsar.handlers.kop.stats.NullStatsLogger;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import org.apache.pulsar.broker.protocol.ProtocolHandler;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
@@ -59,6 +60,7 @@ public class EntryPublishTimeTest extends KopProtocolHandlerTestBase {
                 groupCoordinator,
                 transactionCoordinator,
                 adminManager,
+                pulsar.getLocalMetadataStore().getMetadataCache(LocalBrokerData.class),
                 false,
                 getPlainEndPoint(),
                 NullStatsLogger.INSTANCE);

tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaApisTest.java
@@ -72,6 +72,7 @@ import org.apache.kafka.common.requests.RequestHeader;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.pulsar.broker.protocol.ProtocolHandler;
 import org.apache.pulsar.common.policies.data.RetentionPolicies;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Ignore;
@@ -126,6 +127,7 @@ public class KafkaApisTest extends KopProtocolHandlerTestBase {
             groupCoordinator,
             transactionCoordinator,
             adminManager,
+            pulsar.getLocalMetadataStore().getMetadataCache(LocalBrokerData.class),
             false,
             getPlainEndPoint(),
             NullStatsLogger.INSTANCE);

tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandlerTest.java
@@ -93,6 +93,7 @@ import org.apache.pulsar.common.allocator.PulsarByteBufAllocator;
 import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.policies.data.RetentionPolicies;
 import org.apache.pulsar.common.policies.data.TenantInfo;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -147,6 +148,7 @@ public class KafkaRequestHandlerTest extends KopProtocolHandlerTestBase {
             groupCoordinator,
             transactionCoordinator,
             adminManager,
+            pulsar.getLocalMetadataStore().getMetadataCache(LocalBrokerData.class),
             false,
             getPlainEndPoint(),
             NullStatsLogger.INSTANCE);

tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaTopicConsumerManagerTest.java
@@ -45,6 +45,7 @@ import org.apache.pulsar.broker.protocol.ProtocolHandler;
 import org.apache.pulsar.broker.service.persistent.PersistentTopic;
 import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.common.policies.data.TopicStats;
+import org.apache.pulsar.policies.data.loadbalancer.LocalBrokerData;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -75,6 +76,7 @@ public class KafkaTopicConsumerManagerTest extends KopProtocolHandlerTestBase {
             groupCoordinator,
             transactionCoordinator,
             adminManager,
+            pulsar.getLocalMetadataStore().getMetadataCache(LocalBrokerData.class),
             false,
             getPlainEndPoint(),
             NullStatsLogger.INSTANCE);