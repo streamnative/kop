streamnative/kop

commit f831d3777c82d0b9ee19365ed7ddd7545448185d

add labels for request queued latency and fix request queue size bug (#541)
### Modifications
1. fix request_queue_size guage stats bug
2. add `ALIVE_CHANNEL_COUNT` and `ACTIVE_CHANNEL_COUNT` gauge stats
3. add labels for request queued latency

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -255,6 +255,9 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         this.groupIdStoredPath = kafkaConfig.getGroupIdZooKeeperPath();
         this.maxPendingBytes = kafkaConfig.getMaxMessagePublishBufferSizeInMB() * 1024L * 1024L;
         this.resumeThresholdPendingBytes = this.maxPendingBytes / 2;
+
+        // update alive channel count stats
+        RequestStats.ALIVE_CHANNEL_COUNT_INSTANCE.incrementAndGet();
     }
 
     @Override
@@ -264,12 +267,18 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
         if (authenticator != null) {
             authenticator.reset();
         }
+
+        // update active channel count stats
+        RequestStats.ACTIVE_CHANNEL_COUNT_INSTANCE.incrementAndGet();
         log.info("channel active: {}", ctx.channel());
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         super.channelInactive(ctx);
+
+        // update active channel count stats
+        RequestStats.ACTIVE_CHANNEL_COUNT_INSTANCE.decrementAndGet();
         log.info("channel inactive {}", ctx.channel());
 
         close();
@@ -287,6 +296,9 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
             }
             producePurgatory.shutdown();
             fetchPurgatory.shutdown();
+
+            // update alive channel count stat
+            RequestStats.ACTIVE_CHANNEL_COUNT_INSTANCE.decrementAndGet();
         }
     }
 
@@ -2066,6 +2078,6 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                 .getCounter(MESSAGE_IN)
                 .add(numMessages);
 
-        requestStats.getBatchCountPerMemoryRecords().set(numMessages);
+        RequestStats.BATCH_COUNT_PER_MEMORY_RECORDS_INSTANCE.set(numMessages);
     }
 }

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaCommandDecoder.java
@@ -95,6 +95,9 @@ public abstract class KafkaCommandDecoder extends ChannelInboundHandlerAdapter {
                 // queue is empty
                 break;
             }
+
+            // update request queue size stat
+            RequestStats.REQUEST_QUEUE_SIZE_INSTANCE.decrementAndGet();
         }
         ctx.close();
     }
@@ -198,7 +201,7 @@ public abstract class KafkaCommandDecoder extends ChannelInboundHandlerAdapter {
             });
             // potentially blocking until there is room in the queue for the request.
             requestQueue.put(ResponseAndRequest.of(responseFuture, kafkaHeaderAndRequest));
-            requestStats.getRequestQueueSize().incrementAndGet();
+            RequestStats.REQUEST_QUEUE_SIZE_INSTANCE.incrementAndGet();
 
             if (!isActive.get()) {
                 handleInactive(kafkaHeaderAndRequest, responseFuture);
@@ -347,8 +350,13 @@ public abstract class KafkaCommandDecoder extends ChannelInboundHandlerAdapter {
                 responseFuture.exceptionally(e -> {
                     log.error("[{}] request {} completed exceptionally", channel, request.getHeader(), e);
                     channel.writeAndFlush(request.createErrorResponse(e));
-                    requestStats.getRequestQueuedLatencyStats().registerFailedEvent(
-                            MathUtils.elapsedNanos(responseAndRequest.getCreatedTimestamp()), TimeUnit.NANOSECONDS);
+
+                    requestStats.getStatsLogger()
+                            .scopeLabel(KopServerStats.REQUEST_SCOPE,
+                                    responseAndRequest.request.getHeader().apiKey().name)
+                            .getOpStatsLogger(KopServerStats.REQUEST_QUEUED_LATENCY)
+                            .registerFailedEvent(MathUtils.elapsedNanos(responseAndRequest.getCreatedTimestamp()),
+                                    TimeUnit.NANOSECONDS);
                     return null;
                 }); // send exception to client?
                 continue;
@@ -391,8 +399,12 @@ public abstract class KafkaCommandDecoder extends ChannelInboundHandlerAdapter {
                 responseFuture.cancel(true);
                 channel.writeAndFlush(
                         request.createErrorResponse(new ApiException("request is expired from server side")));
-                requestStats.getRequestQueuedLatencyStats().registerFailedEvent(
-                        MathUtils.elapsedNanos(responseAndRequest.getCreatedTimestamp()), TimeUnit.NANOSECONDS);
+
+                requestStats.getStatsLogger()
+                        .scopeLabel(KopServerStats.REQUEST_SCOPE, responseAndRequest.request.getHeader().apiKey().name)
+                        .getOpStatsLogger(KopServerStats.REQUEST_QUEUED_LATENCY)
+                        .registerFailedEvent(MathUtils.elapsedNanos(responseAndRequest.getCreatedTimestamp()),
+                                TimeUnit.NANOSECONDS);
             }
         }
     }
@@ -616,8 +628,10 @@ public abstract class KafkaCommandDecoder extends ChannelInboundHandlerAdapter {
         }
 
         public void updateStats(final RequestStats requestStats) {
-            requestStats.getRequestQueueSize().decrementAndGet();
-            requestStats.getRequestQueuedLatencyStats()
+            RequestStats.REQUEST_QUEUE_SIZE_INSTANCE.decrementAndGet();
+            requestStats.getStatsLogger()
+                    .scopeLabel(KopServerStats.REQUEST_SCOPE, request.getHeader().apiKey().name)
+                    .getOpStatsLogger(KopServerStats.REQUEST_QUEUED_LATENCY)
                     .registerSuccessfulEvent(MathUtils.elapsedNanos(createdTimestamp), TimeUnit.NANOSECONDS);
         }
 

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KopServerStats.java
@@ -35,6 +35,12 @@ public interface KopServerStats {
     String REQUEST_LATENCY = "REQUEST_LATENCY";
 
     /**
+     * Channel stats.
+     */
+    String ALIVE_CHANNEL_COUNT = "ALIVE_CHANNEL_COUNT";
+    String ACTIVE_CHANNEL_COUNT = "ACTIVE_CHANNEL_COUNT";
+
+    /**
      * Response stats.
      */
     String RESPONSE_BLOCKED_TIMES = "RESPONSE_BLOCKED_TIMES";

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/RequestStats.java
@@ -13,6 +13,8 @@
  */
 package io.streamnative.pulsar.handlers.kop;
 
+import static io.streamnative.pulsar.handlers.kop.KopServerStats.ACTIVE_CHANNEL_COUNT;
+import static io.streamnative.pulsar.handlers.kop.KopServerStats.ALIVE_CHANNEL_COUNT;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.BATCH_COUNT_PER_MEMORYRECORDS;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.CATEGORY_SERVER;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.FETCH_DECODE;
@@ -22,7 +24,6 @@ import static io.streamnative.pulsar.handlers.kop.KopServerStats.MESSAGE_READ;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.PREPARE_METADATA;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.PRODUCE_ENCODE;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.REQUEST_PARSE_LATENCY;
-import static io.streamnative.pulsar.handlers.kop.KopServerStats.REQUEST_QUEUED_LATENCY;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.REQUEST_QUEUE_SIZE;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.RESPONSE_BLOCKED_LATENCY;
 import static io.streamnative.pulsar.handlers.kop.KopServerStats.RESPONSE_BLOCKED_TIMES;
@@ -31,6 +32,7 @@ import static io.streamnative.pulsar.handlers.kop.KopServerStats.SERVER_SCOPE;
 import io.streamnative.pulsar.handlers.kop.stats.StatsLogger;
 import java.util.concurrent.atomic.AtomicInteger;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.bookkeeper.stats.Counter;
 import org.apache.bookkeeper.stats.Gauge;
 import org.apache.bookkeeper.stats.OpStatsLogger;
@@ -46,18 +48,15 @@ import org.apache.bookkeeper.stats.annotations.StatsDoc;
 )
 
 @Getter
+@Slf4j
 public class RequestStats {
 
-    private final AtomicInteger requestQueueSize = new AtomicInteger(0);
-    private final AtomicInteger batchCountPerMemoryRecords = new AtomicInteger(0);
+    public static final AtomicInteger REQUEST_QUEUE_SIZE_INSTANCE = new AtomicInteger(0);
+    public static final AtomicInteger BATCH_COUNT_PER_MEMORY_RECORDS_INSTANCE = new AtomicInteger(0);
+    public static final AtomicInteger ALIVE_CHANNEL_COUNT_INSTANCE = new AtomicInteger(0);
+    public static final AtomicInteger ACTIVE_CHANNEL_COUNT_INSTANCE = new AtomicInteger(0);
 
     private final StatsLogger statsLogger;
-
-    @StatsDoc(
-            name = REQUEST_QUEUED_LATENCY,
-            help = "latency from request enqueued to dequeued"
-    )
-    private final OpStatsLogger requestQueuedLatencyStats;
 
     @StatsDoc(
             name = REQUEST_PARSE_LATENCY,
@@ -116,7 +115,6 @@ public class RequestStats {
     public RequestStats(StatsLogger statsLogger) {
         this.statsLogger = statsLogger;
 
-        this.requestQueuedLatencyStats = statsLogger.getOpStatsLogger(REQUEST_QUEUED_LATENCY);
         this.requestParseLatencyStats = statsLogger.getOpStatsLogger(REQUEST_PARSE_LATENCY);
 
         this.responseBlockedLatency = statsLogger.getOpStatsLogger(RESPONSE_BLOCKED_LATENCY);
@@ -138,7 +136,7 @@ public class RequestStats {
 
             @Override
             public Number getSample() {
-                return requestQueueSize;
+                return REQUEST_QUEUE_SIZE_INSTANCE;
             }
         });
 
@@ -150,7 +148,31 @@ public class RequestStats {
 
             @Override
             public Number getSample() {
-                return batchCountPerMemoryRecords;
+                return BATCH_COUNT_PER_MEMORY_RECORDS_INSTANCE;
+            }
+        });
+
+        statsLogger.registerGauge(ALIVE_CHANNEL_COUNT, new Gauge<Number>() {
+            @Override
+            public Number getDefaultValue() {
+                return 0;
+            }
+
+            @Override
+            public Number getSample() {
+                return ALIVE_CHANNEL_COUNT_INSTANCE;
+            }
+        });
+
+        statsLogger.registerGauge(ACTIVE_CHANNEL_COUNT, new Gauge<Number>() {
+            @Override
+            public Number getDefaultValue() {
+                return 0;
+            }
+
+            @Override
+            public Number getSample() {
+                return ACTIVE_CHANNEL_COUNT_INSTANCE;
             }
         });
     }

tests/src/test/java/io/streamnative/pulsar/handlers/kop/MetricsProviderTest.java
@@ -19,6 +19,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.time.Duration;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.Cleanup;
@@ -157,13 +159,30 @@ public class MetricsProviderTest extends KopProtocolHandlerTestBase{
         InputStreamReader isReader = new InputStreamReader(inputStream);
         BufferedReader reader = new BufferedReader(isReader);
         StringBuffer sb = new StringBuffer();
-        String str;
-        while ((str = reader.readLine()) != null) {
-            if (str.contains("NaN") || str.contains("Infinity")) {
+        String line;
+        Pattern formatPattern = Pattern.compile("^(\\w+)(\\{(\\w+=[\\\"\\.\\w]+(,\\s?\\w+=[\\\"\\.\\w]+)*)\\})?"
+                + "\\s(-?[\\d\\w\\.]+)(\\s(\\d+))?$");
+
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()
+                    || line.startsWith("#")
+                    || line.contains("NaN")
+                    || line.contains("Infinity")) {
                 continue;
             }
-            sb.append(str);
+
+            // check kop metric format
+            if (line.startsWith("kop")) {
+                Matcher formatMatcher = formatPattern.matcher(line);
+                Assert.assertTrue(formatMatcher.matches());
+            }
+
+            sb.append(line);
         }
+
+        // channel stats
+        Assert.assertTrue(sb.toString().contains("kop_server_ALIVE_CHANNEL_COUNT"));
+        Assert.assertTrue(sb.toString().contains("kop_server_ACTIVE_CHANNEL_COUNT"));
 
         // request stats
         Assert.assertTrue(sb.toString().contains("kop_server_REQUEST_QUEUE_SIZE"));
@@ -174,6 +193,8 @@ public class MetricsProviderTest extends KopProtocolHandlerTestBase{
         Assert.assertTrue(sb.toString().contains("request=\"ListOffsets\""));
         Assert.assertTrue(sb.toString().contains("request=\"Fetch\""));
         Assert.assertTrue(sb.toString().contains("kop_server_REQUEST_LATENCY{success=\"true\",quantile=\"0.99\", "
+                + "request=\"Produce\"}"));
+        Assert.assertTrue(sb.toString().contains("kop_server_REQUEST_QUEUED_LATENCY_count{success=\"true\", "
                 + "request=\"Produce\"}"));
 
         // response stats

commit 9c8a099de4c5793c74480df44d36444cdedbfaed

Fix ACTIVE_CHANNEL_COUNT_INSTANCE decrement twice (#719)
This bug was introduced by #541.

## Motivation
The `ACTIVE_CHANNEL_COUNT_INSTANCE` in `close()` should be `ALIVE_CHANNEL_COUNT_INSTANCE`

kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -330,7 +330,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
             fetchPurgatory.shutdown();
 
             // update alive channel count stat
-            RequestStats.ACTIVE_CHANNEL_COUNT_INSTANCE.decrementAndGet();
+            RequestStats.ALIVE_CHANNEL_COUNT_INSTANCE.decrementAndGet();
         }
     }
 