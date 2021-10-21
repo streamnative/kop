c73c4c29f30503056507d55b057ebda0dcaa3dbf
wenbingshen <oliver.shen999@gmail.com>
2021-10-21 14:25:44 +0800

    fix checkstyle

--- kafka-0-9/src/main/java/io/streamnative/kafka/client/zero/nine/Consumer009Impl.java
+++ kafka-0-9/src/main/java/io/streamnative/kafka/client/zero/nine/Consumer009Impl.java
@@ -21,7 +21,6 @@ import io.streamnative.kafka.client.api.TopicOffsetAndMetadata;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
