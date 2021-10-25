bec22304058d95281c52f8c69114ad5c7c931be4
Kai Wang <kwang@streamnative.io>
2021-10-25 09:54:01 +0800

    Bump pulsar version to 2.9.0 (#819)
    ## Motivation
    When we update Pulsar 2.9.0 dependencies in KoP, there will be some compatibility issues. like:
    * getPolicies API has changed
    
    ## Modifications
    * Update Pulsar dependencies to 2.9.0
    * Change new getPolicies API

--- kafka-impl/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaServiceConfigurationTest.java
+++ kafka-impl/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaServiceConfigurationTest.java
@@ -132,7 +132,7 @@ public class KafkaServiceConfigurationTest {
         assertNotNull(kafkaServiceConfig);
         assertEquals(kafkaServiceConfig.getZookeeperServers(), zkServer);
         assertEquals(kafkaServiceConfig.isBrokerDeleteInactiveTopicsEnabled(), true);
-        assertEquals(kafkaServiceConfig.getBacklogQuotaDefaultLimitGB(), 18);
+        assertEquals(kafkaServiceConfig.getBacklogQuotaDefaultLimitGB(), 18.0);
         assertEquals(kafkaServiceConfig.getClusterName(), "usc");
         assertEquals(kafkaServiceConfig.getBrokerClientAuthenticationParameters(), "role:my-role");
         assertEquals(kafkaServiceConfig.getBrokerServicePort().get(), new Integer(7777));
@@ -229,7 +229,7 @@ public class KafkaServiceConfigurationTest {
         NamespaceResources namespaceResources = mock(NamespaceResources.class);
         when(pulsarService.getPulsarResources()).thenReturn(pulsarResources);
         when(pulsarResources.getNamespaceResources()).thenReturn(namespaceResources);
-        when(namespaceResources.getChildrenAsync(any(String.class)))
+        when(namespaceResources.listNamespacesAsync(any(String.class)))
                 .thenReturn(CompletableFuture.completedFuture(Arrays.asList("one", "two")));
         assertEquals(KafkaRequestHandler.expandAllowedNamespaces(conf.getKopAllowedNamespaces(),
                         "logged", pulsarService).get(),
