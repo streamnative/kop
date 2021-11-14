cbc46fbbdd78699414bc31fbd4a1c63c1497fbe4
Yunze Xu <xyzinfernity@163.com>
2021-11-14 22:25:44 +0800

    Make produce and fetch purgatory shared by all connections (#901)
    ### Motivation
    
    Currently each connections holds two `DelayedOperationPurgatory` (purgatory) objects for produce and fetch requests. It's a waste of resources because each purgatory is backed by a time wheel and a reaper thread. In Kafka, these purgatories are held by a `ReplicaManager` instance, which is a global instance shared by all connections.
    
    ### Modifications
    
    Move `producePurgatory` and `fetchPurgatory` into `KafkaProtocolHandler`, and pass them to the constructors of `KafkaChannelInitializer` and `KafkaRequestHandler`. And move the `shutdown` calls from `KafkaRequestHandler#close` to `KafkaProtocolHandler#close`.

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -60,7 +60,6 @@ import io.streamnative.pulsar.handlers.kop.utils.TopicNameUtils;
 import io.streamnative.pulsar.handlers.kop.utils.delayed.DelayedOperation;
 import io.streamnative.pulsar.handlers.kop.utils.delayed.DelayedOperationKey;
 import io.streamnative.pulsar.handlers.kop.utils.delayed.DelayedOperationPurgatory;
-import io.streamnative.pulsar.handlers.kop.utils.timer.SystemTimer;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -221,16 +220,8 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
     // is found.
     private final Map<TopicPartition, PendingTopicFutures> pendingTopicFuturesMap = new ConcurrentHashMap<>();
     // DelayedOperation for produce and fetch
-    private final DelayedOperationPurgatory<DelayedOperation> producePurgatory =
-            DelayedOperationPurgatory.<DelayedOperation>builder()
-                    .purgatoryName("produce")
-                    .timeoutTimer(SystemTimer.builder().executorName("produce").build())
-                    .build();
-    private final DelayedOperationPurgatory<DelayedOperation> fetchPurgatory =
-            DelayedOperationPurgatory.<DelayedOperation>builder()
-                    .purgatoryName("fetch")
-                    .timeoutTimer(SystemTimer.builder().executorName("fetch").build())
-                    .build();
+    private final DelayedOperationPurgatory<DelayedOperation> producePurgatory;
+    private final DelayedOperationPurgatory<DelayedOperation> fetchPurgatory;
 
     // Flag to manage throttling-publish-buffer by atomically enable/disable read-channel.
     private final long maxPendingBytes;
@@ -287,6 +278,8 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                                TenantContextManager tenantContextManager,
                                KopBrokerLookupManager kopBrokerLookupManager,
                                AdminManager adminManager,
+                               DelayedOperationPurgatory<DelayedOperation> producePurgatory,
+                               DelayedOperationPurgatory<DelayedOperation> fetchPurgatory,
                                Boolean tlsEnabled,
                                EndPoint advertisedEndPoint,
                                StatsLogger statsLogger) throws Exception {
@@ -308,6 +301,8 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                 ? new SimpleAclAuthorizer(pulsarService)
                 : null;
         this.adminManager = adminManager;
+        this.producePurgatory = producePurgatory;
+        this.fetchPurgatory = fetchPurgatory;
         this.tlsEnabled = tlsEnabled;
         this.advertisedEndPoint = advertisedEndPoint;
         this.topicManager = new KafkaTopicManager(this);
@@ -357,8 +352,6 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                 log.info("currentConnectedGroup remove {}", clientHost);
                 currentConnectedGroup.remove(clientHost);
             }
-            producePurgatory.shutdown();
-            fetchPurgatory.shutdown();
 
             // update alive channel count stat
             RequestStats.ALIVE_CHANNEL_COUNT_INSTANCE.decrementAndGet();
