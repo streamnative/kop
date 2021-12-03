5bd6af2e1da89ee1b54cb96b38f10c1652622478
chenlin <1572139390@qq.com>
2021-12-03 12:06:27 +0800

    Fix pulsar-io-thread block forever (#946)
    When the requestQueue is full, the pulsar-io thread will always be blocked in: requestQueue.put(), which will cause the requestQueue.remove method to not be called, because the writeAndFlushResponseToClient method is also executed by the pulsar-io thread:
    `                ctx.channel().eventLoop().execute(() -> {
                         writeAndFlushResponseToClient(channel);
                     });`
    This will cause a lot of close_wait on the server side；
    
    related to:
    https://github.com/streamnative/kop/issues/942

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -84,6 +84,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.common.util.OrderedScheduler;
 import org.apache.bookkeeper.mledger.AsyncCallbacks;
 import org.apache.bookkeeper.mledger.ManagedLedgerException;
 import org.apache.bookkeeper.mledger.Position;
@@ -286,8 +287,9 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                                Boolean tlsEnabled,
                                EndPoint advertisedEndPoint,
                                boolean skipMessagesWithoutIndex,
-                               StatsLogger statsLogger) throws Exception {
-        super(statsLogger, kafkaConfig);
+                               StatsLogger statsLogger,
+                               OrderedScheduler sendResponseScheduler) throws Exception {
+        super(statsLogger, kafkaConfig, sendResponseScheduler);
         this.pulsarService = pulsarService;
         this.tenantContextManager = tenantContextManager;
         this.kopBrokerLookupManager = kopBrokerLookupManager;
