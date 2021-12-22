24f3d00a2e1f9e842bb0b3c34da61a527bf254e6
Enrico Olivelli <eolivelli@apache.org>
2021-12-22 07:44:00 +0100

    Fetch: trigger pending fetches when producing messages. (#973)
    When the Kafka client issues a Fetch and sets a maxWait time we are already scheduling a DelayedFetch, but there is no way to trigger that Fetch and it is deemed to wait for the fully timeout.
    This adds latency spikes on the Kafka Consumer.
    
    With this patch we are triggering any pending DelayedFetch in case of writing any record to one of the partitions interested by the Fetch.
    
    This is only a first implementation, in the future we can make it better and do not trigger at the first record, but wait in any case for more records to come.
    With this implementation the Fetch result will contain usually only 1 record, but this is enough to let the Kafka Client start a new Fetch cycle and do not waste time in doing nothing (waiting for maxWait).
    
    Changes:
    - trigger pending Fetches while producing to the topic
    - add new metric WAITING_FETCHES_TRIGGERED
    - add DelayedOperation#wakeup, that means the operation should wake up due to some trigger  (in this case the Production of records to the topic)
    - add a new test that would fail without this patch (because the tests asserts that there is no idle cycle in the Consumer loop)

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/utils/delayed/DelayedOperationPurgatory.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/utils/delayed/DelayedOperationPurgatory.java
@@ -226,7 +226,7 @@ public class DelayedOperationPurgatory<T extends DelayedOperation> {
         if (null == watchers) {
             return 0;
         } else {
-            return watchers.tryCompleteWatched();
+            return watchers.tryCompleteWatched(key);
         }
     }
 
@@ -340,7 +340,7 @@ public class DelayedOperationPurgatory<T extends DelayedOperation> {
         }
 
         // traverse the list and try to complete some watched elements
-        public int tryCompleteWatched() {
+        public int tryCompleteWatched(Object key) {
             int completed = 0;
 
             Iterator<T> iter = operations.iterator();
@@ -349,7 +349,7 @@ public class DelayedOperationPurgatory<T extends DelayedOperation> {
                 if (curr.isCompleted()) {
                     // another thread has completed this operation, just remove it
                     iter.remove();
-                } else if (curr.maybeTryComplete()) {
+                } else if (curr.wakeup() && curr.maybeTryComplete()) {
                     iter.remove();
                     completed += 1;
                 }
