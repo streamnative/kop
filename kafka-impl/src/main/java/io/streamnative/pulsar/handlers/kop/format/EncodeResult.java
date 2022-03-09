023ffeb15a1d45d7839633d17c793ddd146626b1
Demogorgon314 <kwang@streamnative.io>
2022-03-09 15:56:33 +0800

    Fix style check

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/format/EncodeResult.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/format/EncodeResult.java
@@ -22,10 +22,8 @@ import static io.streamnative.pulsar.handlers.kop.KopServerStats.TOPIC_SCOPE;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.util.Recycler;
-import io.streamnative.pulsar.handlers.kop.KafkaTopicManager;
 import io.streamnative.pulsar.handlers.kop.RequestStats;
 import io.streamnative.pulsar.handlers.kop.stats.StatsLogger;
-import io.streamnative.pulsar.handlers.kop.utils.KopTopic;
 import java.util.concurrent.TimeUnit;
 import lombok.Getter;
 import org.apache.kafka.common.TopicPartition;
