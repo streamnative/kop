a07a946fe7f6bfd3f67b949d5ceec5cd69fde2d8
Enrico Olivelli <eolivelli@apache.org>
2022-03-14 15:24:22 +0100

    Refactor: remove static KafkaTopicConsumerManagerCache (#1171)
    ### Motivation
    
    Tests are flaky sometimes due to the fact that we are using stateful objects/cache referred by "static" variables.
    
    ### Modifications
    
    - Move the static state of KafkaTopicManager to KafkaTopicManagerSharedState
    - Create an instance of KafkaTopicManagerSharedState per KafkaProtocolHandler

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaProtocolHandler.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaProtocolHandler.java
@@ -91,8 +91,9 @@ public class KafkaProtocolHandler implements ProtocolHandler, TenantContextManag
 
     @Getter
     private KafkaServiceConfiguration kafkaConfig;
-    @Getter
     private BrokerService brokerService;
+    private KafkaTopicManagerSharedState kafkaTopicManagerSharedState;
+
     @Getter
     private KopEventManager kopEventManager;
     private OrderedScheduler sendResponseScheduler;
@@ -200,6 +201,7 @@ public class KafkaProtocolHandler implements ProtocolHandler, TenantContextManag
             KopVersion.getBuildTime());
 
         brokerService = service;
+        kafkaTopicManagerSharedState = new KafkaTopicManagerSharedState(brokerService);
         PulsarAdmin pulsarAdmin;
         try {
             pulsarAdmin = brokerService.getPulsar().getAdminClient();
@@ -241,9 +243,9 @@ public class KafkaProtocolHandler implements ProtocolHandler, TenantContextManag
             }
 
             private void invalidateBundleCache(TopicName topicName) {
-                KafkaTopicManager.deReference(topicName.toString());
+                kafkaTopicManagerSharedState.deReference(topicName.toString());
                 if (!topicName.isPartitioned()) {
-                    KafkaTopicManager.deReference(topicName.getPartition(0).toString());
+                    kafkaTopicManagerSharedState.deReference(topicName.getPartition(0).toString());
                 }
             }
         });
@@ -392,7 +394,8 @@ public class KafkaProtocolHandler implements ProtocolHandler, TenantContextManag
                 endPoint,
                 kafkaConfig.isSkipMessagesWithoutIndex(),
                 requestStats,
-                sendResponseScheduler);
+                sendResponseScheduler,
+                kafkaTopicManagerSharedState);
     }
 
     // this is called after initialize, and with kafkaConfig, brokerService all set.
@@ -448,10 +451,7 @@ public class KafkaProtocolHandler implements ProtocolHandler, TenantContextManag
         kopEventManager.close();
         transactionCoordinatorByTenant.values().forEach(TransactionCoordinator::shutdown);
         KopBrokerLookupManager.clear();
-        KafkaTopicManager.cancelCursorExpireTask();
-        KafkaTopicConsumerManagerCache.getInstance().close();
-        KafkaTopicManager.getReferences().clear();
-        KafkaTopicManager.getTopics().clear();
+        kafkaTopicManagerSharedState.close();
         kopBrokerLookupManager.close();
         statsProvider.stop();
         sendResponseScheduler.shutdown();
