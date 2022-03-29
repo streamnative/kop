5ebc10939a93321db841158727f3c6f95938d509
Kai Wang <kwang@streamnative.io>
2022-03-29 08:19:14 +0800

    Fix handle offset commit fail when offset producer closed (#1212)
    Fixes #1211
    
    ### Motivation
    Currently, when we get offset producer, then the unload happened, the current producer we get might already close, so the commit offset request will fail.
    
    ### Modifications
    
    1. When the producer closed, we should return Errors.NOT_COORDINATOR.
    2. Add a units test to cover it.
    
    (cherry picked from commit 3c1913fd6ff3a009d24fb447f291d387b2a9a4c4)

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandlerTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandlerTest.java
@@ -55,6 +55,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -74,6 +75,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.Node;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.ConfigResource;
@@ -106,6 +108,7 @@ import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.common.utils.Time;
 import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.common.allocator.PulsarByteBufAllocator;
 import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.policies.data.AutoTopicCreationOverride;
@@ -1173,6 +1176,54 @@ public class KafkaRequestHandlerTest extends KopProtocolHandlerTestBase {
             conf.setAllowAutoTopicCreation(original);
             admin.namespaces().removeAutoTopicCreation("public/default");
         }
+    }
+
+    @Test(timeOut = 30000)
+    public void testCommitOffsetRetryWhenProducerClosed()
+            throws ExecutionException, InterruptedException, PulsarAdminException {
+        String topic = "testCommitOffsetRetryWhenProducerClosed";
+        String groupId = "test-commit-offset-group";
+        admin.topics().createPartitionedTopic(topic, 1);
+        int numMessages = 10;
+        @Cleanup
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(newKafkaProducerProperties());
+        for (int i = 0; i < numMessages; i++) {
+            producer.send(new ProducerRecord<>(topic, "value")).get();
+        }
+
+        @Cleanup
+        final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(newKafkaConsumerProperties(groupId));
+        consumer.subscribe(Collections.singleton(topic));
+
+        int fetchMessages = 0;
+
+        // Make sure only close once.
+        final AtomicBoolean flag = new AtomicBoolean(true);
+
+        while (fetchMessages < numMessages) {
+            try {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                records.forEach(record -> {
+                    consumer.commitSync();
+                    if (flag.get()) {
+                        handler.getGroupCoordinator().getOffsetsProducers().values()
+                                .forEach(producerCompletableFuture -> {
+                            try {
+                                producerCompletableFuture.get().close();
+                            } catch (PulsarClientException | InterruptedException | ExecutionException e) {
+                                log.error("Close offset producer failed.");
+                            }
+                        });
+                        flag.set(false);
+                    }
+                });
+                fetchMessages += records.count();
+            } catch (KafkaException ex) {
+                log.error("Have kafka exception: ", ex);
+                throw ex;
+            }
+        }
+        assertEquals(fetchMessages, numMessages);
     }
 
     private KafkaHeaderAndRequest createTopicMetadataRequest(List<String> topics, boolean allowAutoTopicCreation) {
