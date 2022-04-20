7eb15b83b74d37a7adbb551c7950dc55f8c0f473
Yunze Xu <xyzinfernity@163.com>
2022-04-20 22:27:22 +0800

    Add authorization tests for OAuth2 authentication based on Hydra

---
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/HydraClientInfo.java
@@ -0,0 +1,100 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.SneakyThrows;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class HydraClientInfo {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @JsonProperty("client_id")
+    private String id;
+
+    @JsonProperty("client_secret")
+    private String secret;
+
+    @JsonProperty("audience")
+    private String[] audience;
+
+    @JsonProperty("response_types")
+    private String[] responseTypes = { "code" };
+
+    @JsonProperty("grant_types")
+    private String[] grantTypes = { "client_credentials" };
+
+    // NOTE: Currently KoP only supports "client_secret_post" auth method
+    @JsonProperty("token_endpoint_auth_method")
+    private String tokenEndpointAuthMethod = "client_secret_post";
+
+
+    public static HydraClientInfo[] parseToArray(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, HydraClientInfo[].class);
+    }
+
+    @SneakyThrows
+    @Override
+    public String toString() {
+        return objectMapper.writeValueAsString(this);
+    }
+
+    /**
+     * Write a credential file to the path of the resource directory.
+     *
+     * The content of the file should be a JSON like:
+     *
+     * ```json
+     * {
+     *     "client_id": "xxx",
+     *     "client_secret": "xxx"
+     * }
+     * ```
+     *
+     * @param filename the basename of the file
+     * @return the absolute path of the credential file with a "file://" prefix, which can be configured by Kafka
+     *   clients for OAuth2 authentication
+     * @throws IOException
+     */
+    public String writeCredentials(String filename) throws IOException {
+        final String content = "{\n"
+                + "    \"client_id\": \"" + id + "\",\n"
+                + "    \"client_secret\": \"" + secret + "\"\n"
+                + "}\n";
+        File file = new File(HydraClientInfo.class.getResource("/").getFile() + "/" + filename);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(content);
+        }
+        return "file://" + file.getAbsolutePath();
+    }
+}
--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/SaslOauthKopHandlersWithHydraServerTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/SaslOauthKopHandlersWithHydraServerTest.java
@@ -14,16 +14,29 @@
 package io.streamnative.pulsar.handlers.kop;
 
 import com.google.common.collect.Sets;
-import io.streamnative.pulsar.handlers.kop.security.auth.KafkaMockAuthorizationProvider;
 import io.streamnative.pulsar.handlers.kop.security.oauth.OauthLoginCallbackHandler;
 import io.streamnative.pulsar.handlers.kop.security.oauth.OauthValidatorCallbackHandler;
 import java.net.URL;
-import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import lombok.Cleanup;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.TopicAuthorizationException;
+import org.apache.pulsar.broker.ServiceConfiguration;
+import org.apache.pulsar.broker.authentication.AuthenticationDataSource;
 import org.apache.pulsar.broker.authentication.AuthenticationProviderToken;
+import org.apache.pulsar.broker.authorization.PulsarAuthorizationProvider;
 import org.apache.pulsar.client.admin.PulsarAdmin;
 import org.apache.pulsar.client.impl.auth.oauth2.AuthenticationFactoryOAuth2;
 import org.apache.pulsar.client.impl.auth.oauth2.AuthenticationOAuth2;
+import org.apache.pulsar.common.policies.data.AuthAction;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -40,10 +53,9 @@ public class SaslOauthKopHandlersWithHydraServerTest extends SaslOauthBearerTest
 
     private static final String ADMIN_USER = "simple_client_id";
     private static final String ISSUER_URL = "http://localhost:4444";
-    private static final String CREDENTIALS_URL = "file://"
-            + Paths.get("./src/test/resources/oauth_credentials/simple_credentials_file.json").toAbsolutePath();
     private static final String AUDIENCE = "http://example.com/api/v2/";
 
+    // Generated from ci/hydra/keys/public_key.json
     private static final String TOKEN_PUBLIC_KEY = "data:;base64,MIICIjANBgkqhk"
             + "iG9w0BAQEFAAOCAg8AMIICCgKCAgEA4g8rgGslfLNGdfh94Kbf"
             + "sMPjgX17nnEHnCLhrlVyA+jxSThiQyQVQCkZfav9k4cLCiKdoqxKtLV0RA3hWXGH"
@@ -58,9 +70,16 @@ public class SaslOauthKopHandlersWithHydraServerTest extends SaslOauthBearerTest
             + "xZ8RYWixxrIGc1/8m+QQLy7DwcmVd0dGU29S+fnfOzWr43KWlyWfGsBLFxUkltjY"
             + "6gx6oB6tsQVC3Cy5Eku8FdcCAwEAAQ==";
 
-    @BeforeClass
+    private final HydraHttpClient hydraHttpClient = new HydraHttpClient();
+    private volatile String adminCredentialPath = null;
+
+    @BeforeClass(timeOut = 20000)
     @Override
     protected void setup() throws Exception {
+        final HydraClientInfo adminHydraClient = hydraHttpClient.createClient(
+                ADMIN_USER, "admin_secret", AUDIENCE);
+        adminCredentialPath = adminHydraClient.writeCredentials("admin_credentials.json");
+
         super.resetConfig();
         // Broker's config
         conf.setAuthenticationEnabled(true);
@@ -70,7 +89,7 @@ public class SaslOauthKopHandlersWithHydraServerTest extends SaslOauthBearerTest
         conf.setBrokerClientAuthenticationPlugin(AuthenticationOAuth2.class.getName());
         conf.setBrokerClientAuthenticationParameters(String.format("{\"type\":\"client_credentials\","
                         + "\"privateKey\":\"%s\",\"issuerUrl\":\"%s\",\"audience\":\"%s\"}",
-                CREDENTIALS_URL, ISSUER_URL, AUDIENCE));
+                adminCredentialPath, ISSUER_URL, AUDIENCE));
         final Properties properties = new Properties();
         properties.setProperty("tokenPublicKey", TOKEN_PUBLIC_KEY);
         conf.setProperties(properties);
@@ -86,6 +105,7 @@ public class SaslOauthKopHandlersWithHydraServerTest extends SaslOauthBearerTest
     @AfterClass
     @Override
     protected void cleanup() throws Exception {
+        hydraHttpClient.close();
         super.internalCleanup();
     }
 
@@ -95,7 +115,7 @@ public class SaslOauthKopHandlersWithHydraServerTest extends SaslOauthBearerTest
                 .serviceHttpUrl(brokerUrl.toString())
                 .authentication(
                         AuthenticationFactoryOAuth2.clientCredentials(
-                                new URL(ISSUER_URL), new URL(CREDENTIALS_URL), AUDIENCE))
+                                new URL(ISSUER_URL), new URL(adminCredentialPath), AUDIENCE))
                 .build();
     }
 
@@ -105,32 +125,73 @@ public class SaslOauthKopHandlersWithHydraServerTest extends SaslOauthBearerTest
     }
 
     @Test(timeOut = 15000)
+    public void testGrantAndRevokePermission() throws Exception {
+        final String namespace = conf.getKafkaTenant() + "/" + conf.getKafkaNamespace();
+        final String topic = "test-grant-and-revoke-permission";
+        final String role = "normal-role-" + System.currentTimeMillis();
+        final HydraClientInfo clientInfo = hydraHttpClient.createClient(role, "secret", AUDIENCE);
+        final String credentialsPath = clientInfo.writeCredentials("test-grant-and-revoke-permission.json");
+
+        admin.namespaces().grantPermissionOnNamespace(namespace, role, Collections.singleton(AuthAction.produce));
+        final Properties consumerProps = newKafkaConsumerProperties();
+        internalConfigureOauth2(consumerProps, credentialsPath);
+        @Cleanup
+        final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
+        consumer.subscribe(Collections.singleton(topic));
+        Assert.assertThrows(TopicAuthorizationException.class, () -> consumer.poll(Duration.ofSeconds(2)));
+
+        final Properties producerProps = newKafkaProducerProperties();
+        internalConfigureOauth2(producerProps, credentialsPath);
+        @Cleanup
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps);
+        producer.send(new ProducerRecord<>(topic, "msg-0")).get();
+
+        admin.namespaces().revokePermissionsOnNamespace(namespace, role);
+        try {
+            producer.send(new ProducerRecord<>(topic, "msg-1")).get();
+            Assert.fail(role + " should not have permission to produce");
+        } catch (ExecutionException e) {
+            Assert.assertTrue(e.getCause() instanceof TopicAuthorizationException);
+        }
+
+        admin.namespaces().grantPermissionOnNamespace(namespace, role, Collections.singleton(AuthAction.consume));
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(2));
+        Assert.assertEquals(records.iterator().next().value(), "msg-0");
+    }
+
+    @Test(timeOut = 15000)
     public void testProduceWithoutAuth() throws Exception {
         super.testProduceWithoutAuth();
     }
 
-    @Override
-    protected void configureOauth2(final Properties props) {
+    private void internalConfigureOauth2(final Properties props, final String credentialPath) {
         props.setProperty("sasl.login.callback.handler.class", OauthLoginCallbackHandler.class.getName());
         props.setProperty("security.protocol", "SASL_PLAINTEXT");
         props.setProperty("sasl.mechanism", "OAUTHBEARER");
 
-        final String jassTemplate = "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required"
+        final String jaasTemplate = "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required"
                 + " oauth.issuer.url=\"%s\""
                 + " oauth.credentials.url=\"%s\""
                 + " oauth.audience=\"%s\";";
-        props.setProperty("sasl.jaas.config", String.format(jassTemplate,
+        props.setProperty("sasl.jaas.config", String.format(jaasTemplate,
                 ISSUER_URL,
-                CREDENTIALS_URL,
+                credentialPath,
                 AUDIENCE
         ));
     }
 
-    public static class OauthMockAuthorizationProvider extends KafkaMockAuthorizationProvider {
+    @Override
+    protected void configureOauth2(final Properties props) {
+        internalConfigureOauth2(props, adminCredentialPath);
+    }
+
+    public static class OauthMockAuthorizationProvider extends PulsarAuthorizationProvider {
 
         @Override
-        public boolean roleAuthorized(String role) {
-            return role.equals(ADMIN_USER);
+        public CompletableFuture<Boolean> isSuperUser(String role,
+                                                      AuthenticationDataSource authenticationData,
+                                                      ServiceConfiguration serviceConfiguration) {
+            return CompletableFuture.completedFuture(role.equals(ADMIN_USER));
         }
     }
 }
