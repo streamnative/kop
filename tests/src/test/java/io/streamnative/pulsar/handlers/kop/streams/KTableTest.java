7a232a192ab1e45b67669b22707d8419059e6b8d
Ziyao Wei <ziyao.wei.wzy@gmail.com>
2022-06-17 09:33:23 -0400

    eolivelli and BewareMyPower

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/streams/KTableTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/streams/KTableTest.java
@@ -3,7 +3,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -78,7 +78,6 @@ public class KTableTest extends KafkaStreamsTestBase {
     public void kTableReceivesAndUpdatesValues() throws Exception {
         produceInitialTableValues();
         table.mapValues((k, v) -> {
-            System.out.println(v);
             results.put(String.valueOf(k), v);
             return v;
         });
