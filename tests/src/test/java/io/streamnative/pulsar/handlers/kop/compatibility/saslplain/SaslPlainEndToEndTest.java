a2031245636a2d93cd17eacde8e9784f4cf1f471
Yunze Xu <xyzinfernity@163.com>
2022-06-29 20:18:22 +0800

    Revert "Restore the support for Kafka clients 0.9 and 0.10 (#1307)"
    This reverts commit ffba41ce3cca76100ff01c5e9ef37e91cd1076ff.

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/compatibility/saslplain/SaslPlainEndToEndTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/compatibility/saslplain/SaslPlainEndToEndTest.java
@@ -79,6 +79,7 @@ public class SaslPlainEndToEndTest extends KopProtocolHandlerTestBase {
     private File jaasConfigFile;
 
     protected Map<KafkaVersion, KafkaClientFactory> kafkaClientFactories = Arrays.stream(KafkaVersion.values())
+            .filter(k -> k != KafkaVersion.KAFKA_0_9_0_0 && k != KafkaVersion.KAFKA_0_10_0_0)
             .collect(Collectors.toMap(
                     version -> version,
                     version -> {
