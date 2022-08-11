13957bd370e2c2104e59ded77b2f89e53e6c22f8
Demogorgon314 <kwang@streamnative.io>
2022-08-11 17:49:44 +0800

    Fix test

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaListenerNameTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/KafkaListenerNameTest.java
@@ -20,7 +20,6 @@ import static org.mockito.Mockito.mock;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -66,12 +65,12 @@ public class KafkaListenerNameTest extends KopProtocolHandlerTestBase {
 
     @Test(timeOut = 30000)
     public void testMetadataRequestForMultiListeners() throws Exception {
-        final Map<Integer, InetSocketAddress> bindPortToAdvertisedAddress = new HashMap<>();
+        final Map<Integer, String> bindPortToAdvertisedAddress = new HashMap<>();
         final int anotherKafkaPort = PortManager.nextFreePort();
         bindPortToAdvertisedAddress.put(kafkaBrokerPort,
-                InetSocketAddress.createUnresolved("192.168.0.1", PortManager.nextFreePort()));
+                "192.168.0.1:" + PortManager.nextFreePort());
         bindPortToAdvertisedAddress.put(anotherKafkaPort,
-                InetSocketAddress.createUnresolved("192.168.0.2", PortManager.nextFreePort()));
+                "192.168.0.2:" + PortManager.nextFreePort());
 
         super.resetConfig();
         conf.setKafkaListeners("PLAINTEXT://0.0.0.0:" + kafkaBrokerPort + ",GW://0.0.0.0:" + anotherKafkaPort);
@@ -95,7 +94,7 @@ public class KafkaListenerNameTest extends KopProtocolHandlerTestBase {
                 doReturn(mock(Channel.class)).when(mockCtx).channel();
                 requestHandler.ctx = mockCtx;
 
-                final InetSocketAddress expectedAddress = bindPortToAdvertisedAddress.get(inetSocketAddress.getPort());
+                final String expectedAddress = bindPortToAdvertisedAddress.get(inetSocketAddress.getPort());
 
                 final KafkaHeaderAndRequest metadataRequest = KafkaApisTest.buildRequest(
                         new MetadataRequest.Builder(Collections.singletonList(topic), true),
@@ -106,12 +105,10 @@ public class KafkaListenerNameTest extends KopProtocolHandlerTestBase {
                 Assert.assertEquals(metadataResponse.brokers().size(), 1);
                 final List<Node> brokers = new ArrayList<>(metadataResponse.brokers());
                 Assert.assertEquals(brokers.size(), 1);
-                Assert.assertEquals(brokers.get(0).host(), expectedAddress.getHostName());
-                Assert.assertEquals(brokers.get(0).port(), expectedAddress.getPort());
+                Assert.assertEquals(brokers.get(0).host() + ":" + brokers.get(0).port(), expectedAddress);
 
                 Node controller = metadataResponse.controller();
-                Assert.assertEquals(controller.host(), expectedAddress.getHostName());
-                Assert.assertEquals(controller.port(), expectedAddress.getPort());
+                Assert.assertEquals(controller.host() + ":" + controller.port(), expectedAddress);
 
                 final List<MetadataResponse.TopicMetadata> topicMetadataList =
                         new ArrayList<>(metadataResponse.topicMetadata());
@@ -123,8 +120,8 @@ public class KafkaListenerNameTest extends KopProtocolHandlerTestBase {
                 for (int i = 0; i < numPartitions; i++) {
                     final PartitionMetadata partitionMetadata = partitionMetadataList.get(i);
                     Assert.assertEquals(partitionMetadata.error(), Errors.NONE);
-                    Assert.assertEquals(partitionMetadata.leader().host(), expectedAddress.getHostName());
-                    Assert.assertEquals(partitionMetadata.leader().port(), expectedAddress.getPort());
+                    Assert.assertEquals(partitionMetadata.leader().host() + ":" + partitionMetadata.leader().port(),
+                            expectedAddress);
                 }
             } catch (Exception e) {
                 Assert.fail(e.getMessage());
