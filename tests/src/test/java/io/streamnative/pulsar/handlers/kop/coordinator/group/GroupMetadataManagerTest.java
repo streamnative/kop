a1117dd6580facd973a71bd0115ab92e829497ae
Yunze Xu <xyzinfernity@163.com>
2022-09-19 14:54:04 +0800

    [refactor][group] Simplify code and optimize loading partitions management
    Fixes #1502
    
    ### Motivation
    
    Currently the `partitioner` in `GroupMetadataManager` can be customized
    but it's only for test. The reason is that when
    `testRequestHandlingWhileLoadingInProgress` was migrated from Kafka
    source code, there is something wrong:
    1. `otherGroupId` should be tested while it's actually `groupId`.
    2. `otherGroupId` should be "myGroup", not "myGroupId", which cause the
       error of hash code.
    
    Therefore, a customized partitioner is used in `GroupCoordinatorTest`.
    
    In addition, after https://github.com/apache/kafka/pull/9441, the
    partition of a group id won't be put into `loadPartitions` if it already
    exists in `ownedPartitions`.
    
    ### Modifications
    
    - Don't add partition into `loadPartitions` when it exists in
      `ownedPartitions`.
    - Use the fixed partitioner to calculate the partition of a group id.
      Though the algorithm is still a little different from Kafka.
    - Add the partition to `ownedPartitions` even if loading offsets failed.
    - Add a separate method `removeGroupsAndOffsets` to remove caches and
      close the producer and reader of a partition. In this method, use
      try-finally block for lock instead of `inLock` to avoid usage of
      `AtomicInteger` in a lambda.
    - Fix the wrong tests.

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/coordinator/group/GroupMetadataManagerTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/coordinator/group/GroupMetadataManagerTest.java
@@ -1118,7 +1118,6 @@ public class GroupMetadataManagerTest extends KopProtocolHandlerTestBase {
             .value(buffer)
             .eventTime(Time.SYSTEM.milliseconds())
             .send();
-        groupMetadataManager.removeLoadingPartition(consumerGroupPartitionId);
         CompletableFuture<GroupMetadata> onLoadedFuture = new CompletableFuture<>();
         groupMetadataManager.scheduleLoadGroupAndOffsets(consumerGroupPartitionId,
             groupMetadata -> onLoadedFuture.complete(groupMetadata)
