ac25e4433e70e1f5d2374e013a49fd7686d8d420
Enrico Olivelli <enrico.olivelli@datastax.com>
2022-12-05 05:25:37 +0100

    Transactions - reduce log level (#1598)
    ### Motivation
    
    When you run transaction the broker is full of useless logs
    
    ### Modifications
    
    Reduce the log level on the transactions hot path

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/TransactionMarkerChannelManager.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/TransactionMarkerChannelManager.java
@@ -373,8 +373,10 @@ public class TransactionMarkerChannelManager {
         TransactionMetadata.TxnTransitMetadata newMetadata = pendingCompleteTxn.newMetadata;
         int coordinatorEpoch = pendingCompleteTxn.coordinatorEpoch;
 
-        log.info("Completed sending transaction markers for {}; begin transition to {}",
-                transactionalId, newMetadata.getTxnState());
+        if (log.isDebugEnabled()) {
+            log.debug("Completed sending transaction markers for {}; begin transition to {}",
+                    transactionalId, newMetadata.getTxnState());
+        }
 
         Either<Errors, Optional<TransactionStateManager.CoordinatorEpochAndTxnMetadata>> errorsAndData =
                 txnStateManager.getTransactionState(transactionalId);
@@ -419,8 +421,12 @@ public class TransactionMarkerChannelManager {
                 txnLogAppend.newMetadata, new TransactionStateManager.ResponseCallback() {
             @Override
             public void complete() {
-                log.info("Completed transaction for {} with coordinator epoch {}, final state after commit: {}",
-                    txnLogAppend.transactionalId, txnLogAppend.coordinatorEpoch, txnLogAppend.txnMetadata.getState());
+                if (log.isDebugEnabled()) {
+                    log.debug("Completed transaction for {} with coordinator epoch {}, "
+                                    + "final state after commit: {}",
+                            txnLogAppend.transactionalId, txnLogAppend.coordinatorEpoch,
+                            txnLogAppend.txnMetadata.getState());
+                }
             }
 
             @Override
