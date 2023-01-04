a5a5ce17f1771256bd2e1e37f51f005204d25120
Michael Marshall <mmarshall@apache.org>
2023-01-04 13:23:08 -0600

    Remove unnecessary Boolean boxing

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/TransactionMetadata.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/TransactionMetadata.java
@@ -389,7 +389,7 @@ public class TransactionMetadata {
     public TxnTransitMetadata prepareProducerIdRotation(Long newProducerId,
                                   Integer newTxnTimeoutMs,
                                   Long updateTimestamp,
-                                  Boolean recordLastEpoch) {
+                                  boolean recordLastEpoch) {
         if (hasPendingTransaction()) {
             throw new IllegalStateException("Cannot rotate producer ids while a transaction is still pending");
         }
@@ -491,7 +491,7 @@ public class TransactionMetadata {
         topicPartitions.addAll(partitions);
     }
 
-    public Boolean pendingTransitionInProgress() {
+    public boolean pendingTransitionInProgress() {
         return this.pendingState.isPresent();
     }
 
