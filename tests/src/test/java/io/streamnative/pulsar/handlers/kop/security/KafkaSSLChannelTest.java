504e4781ac531f83b49de2241d9ffd2ea0118234
Enrico Olivelli <eolivelli@apache.org>
2023-01-23 12:18:44 +0100

    [security] Proxy>Broker, Broker>Broker communications: inherit BrokerClient configuration (#54)
    (cherry picked from commit 5ff8b8079c60e6566175faa9640a2312bd95496d)

--- tests/src/test/java/io/streamnative/pulsar/handlers/kop/security/KafkaSSLChannelTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/security/KafkaSSLChannelTest.java
@@ -57,6 +57,7 @@ public class KafkaSSLChannelTest extends KopProtocolHandlerTestBase {
     private String kopClientTruststoreLocation;
     private String kopClientTruststorePassword;
     private final boolean withCertHost;
+    private final boolean useBrokerClientTrustore;
 
     static {
         final HostnameVerifier defaultHostnameVerifier = javax.net.ssl.HttpsURLConnection.getDefaultHostnameVerifier();
@@ -73,9 +74,10 @@ public class KafkaSSLChannelTest extends KopProtocolHandlerTestBase {
         javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(localhostAcceptedHostnameVerifier);
     }
 
-    public KafkaSSLChannelTest(final String entryFormat, boolean withCertHost) {
+    public KafkaSSLChannelTest(final String entryFormat, boolean withCertHost, boolean useBrokerClientTrustore) {
         super(entryFormat);
         this.withCertHost = withCertHost;
+        this.useBrokerClientTrustore = useBrokerClientTrustore;
         setSslConfigurations(withCertHost);
     }
 
@@ -103,10 +105,12 @@ public class KafkaSSLChannelTest extends KopProtocolHandlerTestBase {
     @Factory
     public static Object[] instances() {
         return new Object[] {
-                new KafkaSSLChannelTest("pulsar", false),
-                new KafkaSSLChannelTest("pulsar", true),
-                new KafkaSSLChannelTest("kafka", false),
-                new KafkaSSLChannelTest("kafka", true)
+                new KafkaSSLChannelTest("pulsar", false, false),
+                new KafkaSSLChannelTest("pulsar", true, false),
+                new KafkaSSLChannelTest("kafka", false, false),
+                new KafkaSSLChannelTest("kafka", true, false),
+                // test rokerClientTlsTrustStore
+                new KafkaSSLChannelTest("kafka", true, true)
         };
     }
 
@@ -116,8 +120,13 @@ public class KafkaSSLChannelTest extends KopProtocolHandlerTestBase {
         conf.setKopSslKeystoreType("JKS");
         conf.setKopSslKeystoreLocation(kopSslKeystoreLocation);
         conf.setKopSslKeystorePassword(kopSslKeystorePassword);
-        conf.setKopSslTruststoreLocation(kopSslTruststoreLocation);
-        conf.setKopSslTruststorePassword(kopSslTruststorePassword);
+        if (useBrokerClientTrustore) {
+            conf.setBrokerClientTlsTrustStore(kopSslTruststoreLocation);
+            conf.setBrokerClientTlsTrustStorePassword(kopSslTruststorePassword);
+        } else {
+            conf.setKopSslTruststoreLocation(kopSslTruststoreLocation);
+            conf.setKopSslTruststorePassword(kopSslTruststorePassword);
+        }
     }
 
     @BeforeMethod
