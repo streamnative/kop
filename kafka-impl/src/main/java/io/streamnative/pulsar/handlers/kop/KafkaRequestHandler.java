106b5bd1dc760bb90064c13781b20f31eff24bbd
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-01-23 10:49:41 +0100

    [performance] Remove dummy 10ms throttling on FetchResponse

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/KafkaRequestHandler.java
@@ -1657,7 +1657,7 @@ public class KafkaRequestHandler extends KafkaCommandDecoder {
                                 new FetchResponse<>(
                                     Errors.NONE,
                                     partitions,
-                                    THROTTLE_TIME_MS,
+                                    0,
                                     request.metadata().sessionId()),
                                 () -> resultMap.forEach((__, readRecordsResult) -> {
                                     readRecordsResult.recycle();
