9928dc1d310b513392145e3a3180e3dce221d6a6
Kai Wang <kwang@streamnative.io>
2023-02-16 12:00:42 +0800

    Add multi-tenant support for OAuth authentication (#1728)
    ### Motivation
    
    Currently, KoP stores all group and offset metadata in one topic,
    `public/__kafka/__consumer_offsets`. It's not easy to extend and might
    encounter performance issues for large amount of consumers.
    
    > in the long run as consumers keep increasing as all these consumers
    share the same topic would there be slowness in committing the offsets
    etc.
    
    To solve this issue, we can specify the tenant in `PLAIN`
    authentication's username, for example:
    
    ```
    required username="public/default" password="token:xxx";
    ```
    
    But when using `OAuth` authentication, there is no way to specify the
    tenant.
    
    In this PR, we will introduce a way to specify tenants on OAuth
    authentication, and we will add a new property in
    `credentials_file.json`. For example:
    
    ```json
    {
      "client_id":"Xd23RHsUnvUlP7wchjNYOaIfazgeHd9x",
      "client_secret":"rT7ps7WY8uhdVuBTKWZkttwLdQotmdEliaM5rLfmgNibvqziZ-g07ZH52N_poGAb",
      "tenant": "my_tenant"
    }
    ```
    
    **Internal design**
    
    The tenant will be encoded to a token sent by the client, the token
    format will be `{tenant} __with_tenant__{token}`, since the token only
    allows to `(?<token>[-_\.a-zA-Z0-9]+)`, so here used `__with_tenant__ `
    as the delimiter. On the KoP server side, it will try to extract the
    tenant and token, the tenant will be used as KoP metadata tenant.
    
    ### Modifications
    
    Add multi-tenant support for OAuth authentication.
    
    (cherry picked from commit 55740249efc8ccc02b6d70b43471e51bb64d2b0d)

--- oauth-client/src/test/java/io/streamnative/pulsar/handlers/kop/security/oauth/ClientCredentialsFlowTest.java
+++ oauth-client/src/test/java/io/streamnative/pulsar/handlers/kop/security/oauth/ClientCredentialsFlowTest.java
@@ -13,8 +13,19 @@
  */
 package io.streamnative.pulsar.handlers.kop.security.oauth;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.BoundRequestBuilder;
+import org.asynchttpclient.ListenableFuture;
+import org.asynchttpclient.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -40,5 +51,41 @@ public class ClientCredentialsFlowTest {
         final ClientCredentialsFlow.ClientInfo clientInfo = flow.loadPrivateKey();
         Assert.assertEquals(clientInfo.getId(), "my-id");
         Assert.assertEquals(clientInfo.getSecret(), "my-secret");
+        Assert.assertEquals(clientInfo.getTenant(), "my-tenant");
+    }
+
+    @Test
+    public void testTenantToken() throws ExecutionException, InterruptedException, IOException {
+        AsyncHttpClient mockHttpClient = mock(AsyncHttpClient.class);
+        final ClientCredentialsFlow flow = spy(new ClientCredentialsFlow(ClientConfigHelper.create(
+                "http://localhost:4444",
+                Objects.requireNonNull(
+                        getClass().getClassLoader().getResource("private_key.json")).toString()
+        ), mockHttpClient));
+
+        ClientCredentialsFlow.Metadata mockMetadata = mock(ClientCredentialsFlow.Metadata.class);
+        doReturn("mockTokenEndPoint").when(mockMetadata).getTokenEndPoint();
+
+        doReturn(mockMetadata).when(flow).findAuthorizationServer();
+
+        BoundRequestBuilder mockBuilder = mock(BoundRequestBuilder.class);
+        ListenableFuture<Response> mockFuture = mock(ListenableFuture.class);
+        Response mockResponse = mock(Response.class);
+        doReturn(200).when(mockResponse).getStatusCode();
+        String responseString = "{\n"
+                + "    \"access_token\":\"my-token\",\n"
+                + "    \"expires_in\":42,\n"
+                + "    \"scope\":\"test\"\n"
+                + "}";
+        doReturn(responseString.getBytes()).when(mockResponse).getResponseBodyAsBytes();
+        doReturn(mockResponse).when(mockFuture).get();
+        doReturn(mockFuture).when(mockBuilder).execute();
+        doReturn(mockBuilder).when(mockHttpClient).preparePost(anyString());
+        doReturn(mockBuilder).when(mockBuilder).setHeader(anyString(), anyString());
+        doReturn(mockBuilder).when(mockBuilder).setBody(anyString());
+
+        OAuthBearerTokenImpl token = flow.authenticate();
+        Assert.assertEquals(token.value(), "my-tenant" + OAuthBearerTokenImpl.DELIMITER + "my-token");
+        Assert.assertEquals(token.scope(), Collections.singleton("test"));
     }
 }
---
+++ tests/src/test/java/io/streamnative/pulsar/handlers/kop/security/oauth/OauthValidatorCallbackHandlerTest.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.handlers.kop.security.oauth;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+
+import java.util.HashMap;
+import javax.naming.AuthenticationException;
+import org.apache.pulsar.broker.authentication.AuthenticationProvider;
+import org.apache.pulsar.broker.authentication.AuthenticationService;
+import org.apache.pulsar.broker.authentication.AuthenticationState;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for {@link OauthValidatorCallbackHandler}.
+ */
+public class OauthValidatorCallbackHandlerTest {
+
+    @Test
+    public void testHandleCallback() throws AuthenticationException {
+        AuthenticationService mockAuthService = mock(AuthenticationService.class);
+        OauthValidatorCallbackHandler handler =
+                spy(new OauthValidatorCallbackHandler(new ServerConfig(new HashMap<>()), mockAuthService));
+
+        AuthenticationProvider mockAuthProvider = mock(AuthenticationProvider.class);
+
+        doReturn(mockAuthProvider).when(mockAuthService).getAuthenticationProvider(anyString());
+
+        AuthenticationState state = mock(AuthenticationState.class);
+
+        doReturn(state).when(mockAuthProvider).newAuthState(any(), any(), any());
+
+        KopOAuthBearerValidatorCallback callbackWithTenant =
+                new KopOAuthBearerValidatorCallback("my-tenant" + OAuthTokenDecoder.DELIMITER + "my-token");
+
+        handler.handleCallback(callbackWithTenant);
+
+        KopOAuthBearerToken token = callbackWithTenant.token();
+        assertEquals(token.tenant(), "my-tenant");
+        assertEquals(token.value(), "my-token");
+
+        KopOAuthBearerValidatorCallback callbackWithoutTenant =
+                new KopOAuthBearerValidatorCallback("my-token");
+        handler.handleCallback(callbackWithoutTenant);
+
+        token = callbackWithoutTenant.token();
+        assertNull(token.tenant());
+        assertEquals(token.value(), "my-token");
+    }
+
+}
