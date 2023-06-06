539af5925e3fe3da8d87a1c4ea049405785d9fc8
Kai Wang <kwang@streamnative.io>
2023-06-06 17:34:30 +0800

    [bugfix] Fix decode pulsar format batch records timestamp (#1888)
    ### Motivation
    
    When using pulsar format and has messages In batch, 
    we should use single message metadata's event time as the record
    timestamp.
    
    ### Modifications
    
    Use single message metadata's event time as the record timestamp

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/utils/ByteBufUtils.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/utils/ByteBufUtils.java
@@ -155,8 +155,8 @@ public class ByteBufUtils {
                 final ByteBuf singleMessagePayload = Commands.deSerializeSingleMessageInBatch(
                         uncompressedPayload, singleMessageMetadata, i, numMessages);
 
-                final long timestamp = (metadata.getEventTime() > 0)
-                        ? metadata.getEventTime()
+                final long timestamp = (singleMessageMetadata.getEventTime() > 0)
+                        ? singleMessageMetadata.getEventTime()
                         : metadata.getPublishTime();
                 final ByteBuffer value = singleMessageMetadata.isNullValue()
                         ? null
