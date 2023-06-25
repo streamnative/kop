75b40ab660ca9bba5d42ac2b376d75c267028b8c
Yiming Zang <yzang@twitter.com>
2023-06-24 20:37:21 -0700

    [transactions] log when a TX Coordinator is still loading

--- kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/TransactionStateManager.java
+++ kafka-impl/src/main/java/io/streamnative/pulsar/handlers/kop/coordinator/transaction/TransactionStateManager.java
@@ -568,8 +568,12 @@ public class TransactionStateManager {
         return CoreUtils.inReadLock(stateLock, () -> {
             int partitionId = partitionFor(transactionalId);
             if (loadingPartitions.contains(partitionId)) {
+                log.info("TX Coordinator {} partition {} for transactionalId {} is still loading",
+                    transactionConfig.getTransactionMetadataTopicName(), partitionId, transactionalId);
                 return Either.left(Errors.COORDINATOR_LOAD_IN_PROGRESS);
             } else if (leavingPartitions.contains(partitionId)) {
+                log.info("TX Coordinator {} partition {} for transactionalId {} is unloading",
+                    transactionConfig.getTransactionMetadataTopicName(), partitionId, transactionalId);
                 return Either.left(Errors.NOT_COORDINATOR);
             } else {
                 Map<String, TransactionMetadata> metadataMap = transactionMetadataCache.get(partitionId);
